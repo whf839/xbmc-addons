0d11cba2cbbdab62af9ec6f06e1e215767eab7e5
nuka1195 <nuka1195@753fbab9-553a-0410-a42e-33068982fbc4>
2007-12-06 14:24:00 +0000

    [XBMC-Addons Installer Plugin] - added: initial commit

---
+++ plugins/programs/XBMC-Addons Installer Plugin/default.py
@@ -0,0 +1,22 @@
+"""
+    Plugin for downloading scripts/plugins/skins from http://code.google.com/p/xbmc-addons/
+"""
+
+# main imports
+import sys
+
+# plugin constants
+__plugin__ = "XBMC-Addons Installer"
+__author__ = "nuka1195"
+__url__ = "http://code.google.com/p/xbmc-addons/"
+__svn_url__ = "http://xbmc-addons.googlecode.com/svn/trunk/plugins/programs/XBMC-Addons%20Installer%20Plugin"
+__credits__ = "Team XBMC"
+__version__ = "1.0"
+
+
+if ( __name__ == "__main__" ):
+    if ( "download_url=" in sys.argv[ 2 ] ):
+        from installerAPI import xbmcplugin_downloader as plugin
+    else:
+        from installerAPI import xbmcplugin_list as plugin
+    plugin.Main()
