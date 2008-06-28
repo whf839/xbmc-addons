756ab4895201f05d01a4f3610ce678f04f41b5ac
nuka1195 <nuka1195@753fbab9-553a-0410-a42e-33068982fbc4>
2008-06-28 15:59:04 +0000

    [XBMC-Addons Installer Plugin] - fixed: Skins would not install, if there were any (Thanks Bram77)

--- trunk/plugins/programs/XBMC-Addons Installer/installerAPI/xbmcplugin_list.py
+++ trunk/plugins/programs/XBMC-Addons Installer/installerAPI/xbmcplugin_list.py
@@ -84,7 +84,7 @@ class Main:
             ok = False
             # enumerate through the list of categories and add the item to the media list
             for item in items[ "assets" ]:
-                if ( items[ "url" ].split( "/" )[ -2 ] in ( "plugins", ) or items[ "url" ].split( "/" )[ -1 ] in ( "scripts", "skins" ) ):
+                if ( items[ "url" ].split( "/" )[ -2 ] in ( "plugins", ) or items[ "url" ].split( "/" )[ -1 ] in ( "scripts", "skin" ) ):
                     heading = "download_url"
                     thumbnail = self._get_thumbnail( "%s%s/%sdefault.tbn" % ( self.BASE_URL, items[ "url" ], item, ) )
                     isFolder = False
