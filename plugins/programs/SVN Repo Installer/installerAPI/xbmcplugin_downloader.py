5b5f90d1f4604a3b8634244ac82461faf2ea342b
nuka1195 <nuka1195@753fbab9-553a-0410-a42e-33068982fbc4>
2008-10-16 04:05:04 +0000

    [SVN Repo Installer] - changed: version -> 1.1 - fixed: items with & in there name did not show - changed: repo.xml format and updated the current repos
    format of repo.xml
    url == url of repository
    root == root folder of repo
    structures:
         name == folder name to start listing items
         noffset == offset from item where name appears (negative index number starts from the end and moves backwards)
         install == additional path needed to install item
         ioffset == index of split url to include in install path

--- plugins/programs/SVN Repo Installer/installerAPI/xbmcplugin_downloader.py
+++ plugins/programs/SVN Repo Installer/installerAPI/xbmcplugin_downloader.py
@@ -12,35 +12,45 @@ import xbmcgui
 import xbmcplugin
 
 import urllib
-from sgmllib import SGMLParser
 import re
+from xml.sax.saxutils import unescape
 
 
-class Parser( SGMLParser ):
+class Parser:
     """ Parser Class: grabs all tag versions and urls """
-    def reset( self ):
-        self.tags = []
-        self.dict = { "status": "fail"}
-        self.tag_found = None
-        self.url_found = True
-        SGMLParser.reset( self )
+    # regexpressions
+    revision_regex = re.compile( '<h2>Revision ([0-9]*): ([^<]*)</h2>' )
+    asset_regex = re.compile( '<li><a href="([^"]*)">([^"]*)</a></li>' )
 
-    def start_a( self, attrs ):
-        for key, value in attrs:
-            if ( key == "href" ): self.tag_found = value
-    
-    def handle_data( self, text ):
-        if ( self.tag_found == text.replace( " ", "%20" ) ):
-            self.tags.append( self.tag_found )
-            self.tag_found = False
-        if ( self.url_found ):
-            self.dict[ "items" ] = { "url": text.split( ":" )[ 1 ].strip(), "assets": self.tags }
-            self.dict[ "status" ] = "ok"
-            self.url_found = False
-            
-    def unknown_starttag( self, tag, attrs ):
-        if ( tag == "h2" ):
-            self.url_found = True
+    def __init__( self, htmlSource ):
+        # set our initial status
+        self.dict = { "status": "fail", "revision": 0, "assets": [], "url": "" }
+        # fetch revision number
+        self._fetch_revision( htmlSource )
+        # if we were successful, fetch assets
+        if ( self.dict[ "revision" ] != 0 ):
+            self._fetch_assets( htmlSource )
+
+    def _fetch_revision( self, htmlSource ):
+        try:
+            # parse revision and current dir level
+            revision, url = self.revision_regex.findall( htmlSource )[ 0 ]
+            # we succeeded :), set our info
+            self.dict[ "url" ] = url
+            self.dict[ "revision" ] = int( revision )
+        except:
+            pass
+
+    def _fetch_assets( self, htmlSource ):
+        try:
+            assets = self.asset_regex.findall( htmlSource )
+            if ( len( assets ) ):
+                for asset in assets:
+                    if ( asset[ 0 ] != "../" ):
+                        self.dict[ "assets" ] += [ unescape( asset[ 0 ] ) ]
+                self.dict[ "status" ] = "ok"
+        except:
+            pass
 
 
 class _Info:
@@ -62,7 +72,7 @@ class Main:
 
     def _get_repo_info( self ):
         # path to info file
-        repopath = xbmc.translatePath( os.path.join( os.getcwd().replace( ";", "" ), "resources", "repositories", xbmcplugin.getSetting( "repository" ), "repo.xml" ) )
+        repopath = os.path.join( os.getcwd(), "resources", "repositories", xbmcplugin.getSetting( "repository" ), "repo.xml" )
         try:
             # grab a file object
             fileobject = open( repopath, "r" )
@@ -79,12 +89,13 @@ class Main:
     def _parse_argv( self ):
         # call _Info() with our formatted argv to create the self.args object
         exec "self.args = _Info(%s)" % ( sys.argv[ 2 ][ 1 : ].replace( "&", ", " ).replace( "\\u0027", "'" ).replace( "\\u0022", '"' ).replace( "\\u0026", "&" ), )
+        self.args.download_url = urllib.unquote_plus( self.args.download_url )
 
     def _download_item( self ):
         try:
             if ( xbmcgui.Dialog().yesno( self.title, xbmc.getLocalizedString( 30000 ), "", "", xbmc.getLocalizedString( 30020 ), xbmc.getLocalizedString( 30021 ) ) ):
                 self.dialog.create( self.title, xbmc.getLocalizedString( 30002 ), xbmc.getLocalizedString( 30003 ) )
-                script_files = []
+                asset_files = []
                 folders = [ self.args.download_url ]
                 while folders:
                     try:
@@ -94,41 +105,36 @@ class Main:
                         if ( not items or items[ "status" ] == "fail" ): raise
                         files, dirs = self._parse_items( items )
                         for file in files:
-                            script_files.append( "%s/%s" % ( items[ "items" ][ "url" ], file, ) )
+                            asset_files.append( "%s/%s" % ( items[ "url" ], file, ) )
                         for folder in dirs:
                             folders.append( folders[ 0 ] + folder )
                         folders = folders[ 1 : ]
                     except:
                         folders = []
-                self._get_files( script_files )
+                self._get_files( asset_files )
         except:
             # oops print error message
             print "ERROR: %s::%s (%d) - %s" % ( self.__class__.__name__, sys.exc_info()[ 2 ].tb_frame.f_code.co_name, sys.exc_info()[ 2 ].tb_lineno, sys.exc_info()[ 1 ], )
             self.dialog.close()
             xbmcgui.Dialog().ok( self.title, xbmc.getLocalizedString( 30030 ) )
         
-    def _get_files( self, script_files ):
+    def _get_files( self, asset_files ):
         """ fetch the files """
         try:
             finished_path = ""
-            for cnt, url in enumerate( script_files ):
+            for cnt, url in enumerate( asset_files ):
                 items = os.path.split( url )
                 # TODO: Change this to U: for other than xbox
-                drive = ( "U:\\%s" % self.args.install, "Q:\\%s" % self.args.install, )[ os.environ.get( "OS", "xbox" ) == "xbox" ]
-                path = drive + items[ 0 ][ 7 : ].replace( "%20", " " ).replace( "/", "\\" )
-                # use this if we categorize scripts
-                #if ( path.startswith( "Q:\\scripts" ) ):
-                #    path = path.split( "\\" )
-                #    path = path[ : 2 ] + path[ 3 : ]
-                #    path = "\\".join( path )
-                if ( not finished_path ): finished_path = xbmc.translatePath( path )
+                drive = xbmc.translatePath( ( "U:\\%s" % self.args.install, "Q:\\%s" % self.args.install, )[ os.environ.get( "OS", "xbox" ) == "xbox" ] )
+                path = os.path.join( drive, os.path.sep.join( items[ 0 ].split( "/" )[ self.args.ioffset : ] ).replace( "%20", " " ) )
+                if ( not finished_path ): finished_path = path
                 file = items[ 1 ].replace( "%20", " " )
-                pct = int( ( float( cnt ) / len( script_files ) ) * 100 )
-                self.dialog.update( pct, "%s %s" % ( xbmc.getLocalizedString( 30005 ), url, ), "%s %s" % ( xbmc.getLocalizedString( 30006 ), xbmc.translatePath( path ), ), "%s %s" % ( xbmc.getLocalizedString( 30007 ), file, ) )
+                pct = int( ( float( cnt ) / len( asset_files ) ) * 100 )
+                self.dialog.update( pct, "%s %s" % ( xbmc.getLocalizedString( 30005 ), url, ), "%s %s" % ( xbmc.getLocalizedString( 30006 ), path, ), "%s %s" % ( xbmc.getLocalizedString( 30007 ), file, ) )
                 if ( self.dialog.iscanceled() ): raise
-                if ( not os.path.isdir( xbmc.translatePath( path ) ) ): os.makedirs( xbmc.translatePath( path ) )
+                if ( not os.path.isdir( path ) ): os.makedirs( path )
                 url = self.REPO_URL + url
-                fpath = xbmc.translatePath( os.path.join( path, file ) )
+                fpath = os.path.join( path, file )
                 urllib.urlretrieve( url.replace( " ", "%20" ), fpath )
         except:
             # oops print error message
@@ -150,9 +156,9 @@ class Main:
     def _parse_html_source( self, htmlsource ):
         """ parse html source for tagged version and url """
         try:
-            parser = Parser()
-            parser.feed( htmlsource )
-            parser.close()
+            parser = Parser( htmlsource )
+            #parser.feed( htmlsource )
+            #parser.close()
             return parser.dict
         except:
             return {}
@@ -161,7 +167,7 @@ class Main:
         """ separates files and folders """
         folders = []
         files = []
-        for item in items[ "items" ][ "assets" ]:
+        for item in items[ "assets" ]:
             if ( item.endswith( "/" ) ):
                 folders.append( item )
             else:
