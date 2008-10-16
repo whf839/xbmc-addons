5e6e89e13d755c6c34371264650813f8cf9034bb
nuka1195 <nuka1195@753fbab9-553a-0410-a42e-33068982fbc4>
2008-10-16 04:05:04 +0000

    [SVN Repo Installer] - changed: version -> 1.1 - fixed: items with & in there name did not show - changed: repo.xml format and updated the current repos
    format of repo.xml
    url == url of repository
    root == root folder of repo
    structures:
         name == folder name to start listing items
         noffset == offset from item where name appears (negative index number starts from the end and moves backwards)
         install == additional path needed to install item
         ioffset == index of split url to include in install path

--- trunk/plugins/programs/SVN Repo Installer/installerAPI/xbmcplugin_list.py
+++ trunk/plugins/programs/SVN Repo Installer/installerAPI/xbmcplugin_list.py
@@ -12,35 +12,45 @@ import xbmcgui
 import xbmcplugin
 
 import urllib
-from sgmllib import SGMLParser
 import re
+from xml.sax.saxutils import unescape
 
 
-class Parser( SGMLParser ):
+class Parser:
     """ Parser Class: grabs all tag versions and urls """
-    def reset( self ):
-        self.tags = []
-        self.dict = { "status": "fail"}
-        self.tag_found = None
-        self.url_found = True
-        SGMLParser.reset( self )
+    # regexpressions
+    revision_regex = re.compile( '<h2>Revision ([0-9]*): ([^<]*)</h2>' )
+    asset_regex = re.compile( '<li><a href="([^"]*)">([^"]*)</a></li>' )
 
-    def start_a( self, attrs ):
-        for key, value in attrs:
-            if ( key == "href" ): self.tag_found = value
-    
-    def handle_data( self, text ):
-        if ( self.tag_found == text.replace( " ", "%20" ) ):
-            self.tags.append( self.tag_found )
-            self.tag_found = False
-        if ( self.url_found ):
-            self.dict[ "items" ] = { "url": text.split( ":" )[ 1 ].strip(), "assets": self.tags }
-            self.dict[ "status" ] = "ok"
-            self.url_found = False
-            
-    def unknown_starttag( self, tag, attrs ):
-        if ( tag == "h2" ):
-            self.url_found = True
+    def __init__( self, htmlSource ):
+        # set our initial status
+        self.dict = { "status": "fail", "revision": 0, "assets": [], "url": "" }
+        # fetch revision number
+        self._fetch_revision( htmlSource )
+        # if we were successful, fetch assets
+        if ( self.dict[ "revision" ] != 0 ):
+            self._fetch_assets( htmlSource )
+
+    def _fetch_revision( self, htmlSource ):
+        try:
+            # parse revision and current dir level
+            revision, url = self.revision_regex.findall( htmlSource )[ 0 ]
+            # we succeeded :), set our info
+            self.dict[ "url" ] = url
+            self.dict[ "revision" ] = int( revision )
+        except:
+            pass
+
+    def _fetch_assets( self, htmlSource ):
+        try:
+            assets = self.asset_regex.findall( htmlSource )
+            if ( len( assets ) ):
+                for asset in assets:
+                    if ( asset[ 0 ] != "../" ):
+                        self.dict[ "assets" ] += [ unescape( asset[ 0 ] ) ]
+                self.dict[ "status" ] = "ok"
+        except:
+            pass
 
 
 class _Info:
@@ -50,7 +60,7 @@ class _Info:
 
 class Main:
     # base path
-    BASE_CACHE_PATH = os.path.join( "P:\\Thumbnails", "Pictures" )
+    BASE_CACHE_PATH = os.path.join( xbmc.translatePath( "P:\\Thumbnails" ), "Pictures" )
 
     def __init__( self ):
         # get the repository info
@@ -66,6 +76,7 @@ class Main:
         # call _Info() with our formatted argv to create the self.args object
         if ( sys.argv[ 2 ] ):
             exec "self.args = _Info(%s)" % ( sys.argv[ 2 ][ 1 : ].replace( "&", ", " ).replace( "\\u0027", "'" ).replace( "\\u0022", '"' ).replace( "\\u0026", "&" ), )
+            self.args.category = urllib.unquote_plus( self.args.category )
         else:
             self.args = _Info( category=self.REPO_ROOT )
 
@@ -84,7 +95,7 @@ class Main:
             # root of repository
             self.REPO_ROOT = re.findall( '<root>([^<]*)</root>', info )[ 0 ]
             # structure of repo
-            self.REPO_STRUCTURES = re.findall( '<structure name="([^"]+)" offset="([^"]+)" install="([^"]*)"', info )
+            self.REPO_STRUCTURES = re.findall( '<structure name="([^"]+)" noffset="([^"]+)" install="([^"]*)" ioffset="([^"]+)"', info )
         except:
             # oops print error message
             print "ERROR: %s::%s (%d) - %s" % ( self.__class__.__name__, sys.exc_info()[ 2 ].tb_frame.f_code.co_name, sys.exc_info()[ 2 ].tb_lineno, sys.exc_info()[ 1 ], )
@@ -96,17 +107,17 @@ class Main:
         # if successful
         if ( items and items[ "status" ] == "ok" ):
             # if there are assets, we have categories
-            ok = self._fill_list( items[ "items" ] )
+            ok = self._fill_list( items[ "url" ], items[ "revision" ], items[ "assets" ] )
         return ok
 
-    def _fill_list( self, items ):
+    def _fill_list( self, repo_url, revision, assets ):
         try:
             ok = False
             # enumerate through the list of categories and add the item to the media list
-            for item in items[ "assets" ]:
+            for item in assets:
                 isFolder = True
-                for name, offset, install in self.REPO_STRUCTURES:
-                    if ( items[ "url" ].split( "/" )[ int( offset ) ] == name ):
+                for name, noffset, install, ioffset in self.REPO_STRUCTURES:
+                    if ( repo_url.split( "/" )[ int( noffset ) ].lower() == name.lower() ):
                         isFolder = False
                         break
                 if ( isFolder ):
@@ -114,16 +125,16 @@ class Main:
                     thumbnail = ""
                 else:
                     heading = "download_url"
-                    thumbnail = self._get_thumbnail( "%s%s/%sdefault.tbn" % ( self.REPO_URL, items[ "url" ], item, ) )
-                url = '%s?%s="%s/%s"&install="%s"' % ( sys.argv[ 0 ], heading, items[ "url" ], item, install )
+                    thumbnail = self._get_thumbnail( "%s%s/%sdefault.tbn" % ( self.REPO_URL, repo_url, item, ) )
+                url = '%s?%s="%s/%s"&install="%s"&ioffset=%s' % ( sys.argv[ 0 ], heading, urllib.quote_plus( repo_url ), urllib.quote_plus( item ), install, ioffset, )
                 # set the default icon
                 icon = "DefaultFolder.png"
                 # create our listitem, fixing title
-                listitem = xbmcgui.ListItem( item[ : -1 ].replace( "%20", " " ).title(), iconImage=icon, thumbnailImage=thumbnail )
+                listitem = xbmcgui.ListItem( urllib.unquote( item[ : -1 ] ).title(), iconImage=icon, thumbnailImage=thumbnail )
                 # set the title
-                listitem.setInfo( type="Video", infoLabels={ "Title": item[ : -1 ].replace( "%20", " " ).title() } )
+                listitem.setInfo( type="Video", infoLabels={ "Title": urllib.unquote( item[ : -1 ] ).title() } )
                 # add the item to the media list
-                ok = xbmcplugin.addDirectoryItem( handle=int( sys.argv[ 1 ] ), url=url, listitem=listitem, isFolder=isFolder, totalItems=len( items[ "assets" ] ) )
+                ok = xbmcplugin.addDirectoryItem( handle=int( sys.argv[ 1 ] ), url=url, listitem=listitem, isFolder=isFolder, totalItems=len( assets ) )
                 # if user cancels, call raise to exit loop
                 if ( not ok ): raise
         except:
@@ -149,13 +160,9 @@ class Main:
             print "ERROR: %s::%s (%d) - %s" % ( self.__class__.__name__, sys.exc_info()[ 2 ].tb_frame.f_code.co_name, sys.exc_info()[ 2 ].tb_lineno, sys.exc_info()[ 1 ], )
             return {}
 
-    def _parse_html_source( self, htmlsource ):
+    def _parse_html_source( self, htmlSource ):
         # initialize the parser
-        parser = Parser()
-        # pass the htmlSource
-        parser.feed( htmlsource )
-        # close parser
-        parser.close()
+        parser = Parser( htmlSource )
         # return results
         return parser.dict
 
@@ -164,7 +171,7 @@ class Main:
         if ( not thumbnail_url.startswith( "http://" ) ): return thumbnail_url
         try:
             filename = xbmc.getCacheThumbName( thumbnail_url )
-            filepath = xbmc.translatePath( os.path.join( self.BASE_CACHE_PATH, filename[ 0 ], filename ) )
+            filepath = os.path.join( self.BASE_CACHE_PATH, filename[ 0 ], filename )
             # if the cached thumbnail does not exist fetch the thumbnail
             if ( not os.path.isfile( filepath ) ):
                 # fetch thumbnail and save to filepath
