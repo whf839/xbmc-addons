68f6c520b641c81e9202a80b7b2c771e847a9ec8
nuka1195@gmail.com <nuka1195@gmail.com@753fbab9-553a-0410-a42e-33068982fbc4>
2009-02-20 21:22:36 +0000

    [SVN Repo Installer]
    -changed: version -> 1.4
    -changed: uses new special:// paths

--- plugins/programs/SVN Repo Installer/default.py
+++ plugins/programs/SVN Repo Installer/default.py
@@ -11,7 +11,7 @@ __author__ = "nuka1195"
 __url__ = "http://code.google.com/p/xbmc-addons/"
 __svn_url__ = "http://xbmc-addons.googlecode.com/svn/trunk/plugins/programs/SVN%20Repo%20Installer"
 __credits__ = "Team XBMC"
-__version__ = "1.3.1"
+__version__ = "1.4"
 
 
 if ( __name__ == "__main__" ):
