38cab30827faed455156a57b5a17fa4c1b57302a
nuka1195@gmail.com <nuka1195@gmail.com@753fbab9-553a-0410-a42e-33068982fbc4>
2009-02-28 00:23:31 +0000

    [Picasa]
    -changed: version -> 1.3
    -changed: save presets in users settings.xml file

--- trunk/plugins/pictures/Picasa/PicasaAPI/xbmcplugin_categories.py
+++ trunk/plugins/pictures/Picasa/PicasaAPI/xbmcplugin_categories.py
@@ -19,12 +19,8 @@ class Main:
     # base paths
     BASE_SKIN_THUMBNAIL_PATH = "/".join( [ "special://xbmc", "skin", xbmc.getSkinDir(), "media", sys.modules[ "__main__" ].__plugin__ ] )
     BASE_PLUGIN_THUMBNAIL_PATH = os.path.join( os.getcwd(), "thumbnails" )
-    BASE_PRESETS_PATH = "/".join( [ "special://profile", "plugin_data", "pictures", os.path.basename( os.getcwd() ) ] )
-    BASE_AUTH_FILE_PATH = "/".join( [ "special://profile", "plugin_data", "pictures", os.path.basename( os.getcwd() ), "authfile.txt" ] )
 
     def __init__( self ):
-        # create the settings folder for presets
-        self.make_presets_folders()
         # parse sys.argv
         self._parse_argv()
         # authenticate user
@@ -33,10 +29,6 @@ class Main:
             self.get_categories()
         else:
             self.get_categories( False )
-
-    def make_presets_folders( self ):
-        if ( not os.path.isdir( xbmc.translatePath( self.BASE_PRESETS_PATH ) ) ):
-            os.makedirs( xbmc.translatePath( self.BASE_PRESETS_PATH ) )
 
     def _parse_argv( self ):
         if ( not sys.argv[ 2 ] ):
@@ -82,10 +74,10 @@ class Main:
             # default categories
             if ( root ):
                 categories = (
-                                        ( xbmc.getLocalizedString( 30950 ), "users_albums", "", "", True, 0, "album", "", "", True, 0, ),
-                                        ( xbmc.getLocalizedString( 30951 ), "presets_photos", "", "", True, 0, "photo", "", "", False, 0, ),
-                                        ( xbmc.getLocalizedString( 30952 ), "presets_users", "", "", True, 0, "album", "", "", False, 0, ),
-                                        ( xbmc.getLocalizedString( 30955 ), "users_photos", "", "", True, 0, "photo", "", "", True, 0, ),
+                                        ( xbmc.getLocalizedString( 30950 ), "users_albums", "", "", True, 0, "album", "", "", True, ),
+                                        ( xbmc.getLocalizedString( 30951 ), "presets_photos", "", "", True, 0, "photo", "", "", False, ),
+                                        ( xbmc.getLocalizedString( 30952 ), "presets_users", "", "", True, 0, "album", "", "", False, ),
+                                        ( xbmc.getLocalizedString( 30955 ), "users_photos", "", "", True, 0, "photo", "", "", True, ),
                                     )
             # photo preset category
             elif ( "category='presets_photos'" in sys.argv[ 2 ] ):
@@ -105,25 +97,17 @@ class Main:
         xbmcplugin.endOfDirectory( handle=int( sys.argv[ 1 ] ), succeeded=ok, cacheToDisc=cacheToDisc )
 
     def get_presets( self, ptype=False ):
-        # we add time to the url, so it is always unique
-        import time
-        # set the proper preset path
-        preset_path = "/".join( [ self.BASE_PRESETS_PATH, ( "photos", "users", )[ ptype ] ] )
         # initialize our category tuple
         categories = ()
         # add our new search item
         if ( ptype ):
-            categories += ( ( xbmc.getLocalizedString( 30953 ), "search_users", "", "", True, 1, "album", "", "", False, time.time(), ), )
+            categories += ( ( xbmc.getLocalizedString( 30953 ), "search_users", "", "", True, 2, "album", "", "", False, ), )
         else:
-            categories += ( ( xbmc.getLocalizedString( 30954 ), "search_photos", "", "", True, 2, "photo", "", "", False, time.time(), ), )
+            categories += ( ( xbmc.getLocalizedString( 30954 ), "search_photos", "", "", True, 1, "photo", "", "", False, ), )
         # fetch saved presets
         try:
-            # grab a file object
-            fileobject = open( preset_path, "r" )
             # read the queries
-            presets = eval( fileobject.read() )
-            # close file object
-            fileobject.close()
+            presets = eval( xbmcplugin.getSetting( "presets_%s" % ( "photos", "users", )[ ptype ], ) )
             # sort items
             presets.sort()
         except:
@@ -149,7 +133,7 @@ class Main:
                 access = ( "all", "private", "public", )[ int( xbmcplugin.getSetting( "access" ) ) ]
                 access = ( access, "public", )[ category == "photos" ]
                 # add preset to our dictionary
-                categories += ( ( query.split( " | " )[ 0 ].encode( "utf-8" ), category, pq, username, True, 0, kind, access, thumbnail, False, 0, ), )
+                categories += ( ( query.split( " | " )[ 0 ].encode( "utf-8" ), category, pq, username, True, 0, kind, access, thumbnail, False, ), )
             except:
                 # oops print error message
                 print "ERROR: %s::%s (%d) - %s" % ( self.__class__.__name__, sys.exc_info()[ 2 ].tb_frame.f_code.co_name, sys.exc_info()[ 2 ].tb_lineno, sys.exc_info()[ 1 ], )
@@ -159,22 +143,17 @@ class Main:
         try:
             ok = True
             # enumerate through the tuple of categories and add the item to the media list
-            for ( ltitle, method, pq, username, isfolder, issearch, kind, access, thumbnail, login_required, time, ) in categories:
+            for ( ltitle, method, pq, username, isfolder, issearch, kind, access, thumbnail, login_required, ) in categories:
                 # if a user id is required for category and none supplied, skip category
                 if ( login_required and self.email == "" ): continue
                 # set the callback url
-                url = '%s?title=%s&category=%s&access=%s&kind=%s&page=1&user_id=%s&album_id=%s&photo_id=%s&pq=%s&issearch=%d&update_listing=%d&time=%d' % ( sys.argv[ 0 ], repr( ltitle ), repr( method ), repr( access ), repr( kind ), repr( username ), repr( "" ), repr( "" ), repr( pq ), issearch, False, time, )
+                url = '%s?title=%s&category=%s&access=%s&kind=%s&page=1&user_id=%s&album_id=%s&photo_id=%s&pq=%s&issearch=%d&update_listing=%d' % ( sys.argv[ 0 ], repr( ltitle ), repr( method ), repr( access ), repr( kind ), repr( username ), repr( "" ), repr( "" ), repr( pq ), issearch, False, )
                 # check for a valid custom thumbnail for the current category
                 thumbnail = thumbnail or self._get_thumbnail( method )
                 # set the default icon
                 icon = "DefaultFolderBig.png"
                 # only need to add label, icon and thumbnail, setInfo() and addSortMethod() takes care of label2
                 listitem=xbmcgui.ListItem( ltitle, iconImage=icon, thumbnailImage=thumbnail )
-                # set special properties
-                listitem.setProperty( "IsFolder", "true" )
-                listitem.setProperty( "IsPictureFolder", "true" )
-                if ( time > 0 ):
-                    listitem.setProperty( "NoHistory", "1" )
                 # add the item to the media list
                 ok = xbmcplugin.addDirectoryItem( handle=int( sys.argv[ 1 ] ), url=url, listitem=listitem, isFolder=isfolder, totalItems=len( categories ) )
                 # if user cancels, call raise to exit loop
