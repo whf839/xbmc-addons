458c98f9170894ecdd61e05743cce4f6f279988a
nuka1195@gmail.com <nuka1195@gmail.com@753fbab9-553a-0410-a42e-33068982fbc4>
2009-03-01 20:22:14 +0000

    [Youtube]
    -changed: version -> 1.1
    -changed: english language strings
    -added: authentication
    -changed: save presets in users settings.xml file
    -changed: use xbmc.skinHasImage() check for category icons. (They need to be in skinname/media/Youtube/, named identical to the method name with a .png ext. eg "recently_featured.png")

--- plugins/video/Youtube/YoutubeAPI/xbmcplugin_categories.py
+++ plugins/video/Youtube/YoutubeAPI/xbmcplugin_categories.py
@@ -17,25 +17,17 @@ class _Info:
 
 class Main:
     # base paths
-    BASE_SKIN_THUMBNAIL_PATH = "/".join( [ "special://xbmc", "skin", xbmc.getSkinDir(), "media", sys.modules[ "__main__" ].__plugin__ ] )
     BASE_PLUGIN_THUMBNAIL_PATH = os.path.join( os.getcwd(), "thumbnails" )
-    BASE_PRESETS_PATH = "/".join( [ "special://profile", "plugin_data", "video", os.path.basename( os.getcwd().replace( ";", "" ) ) ] )
 
     def __init__( self ):
-        # create the settings folder for presets
-        self.make_presets_folders()
         # parse sys.argv
         self._parse_argv()
-        # set username
-        self.username = xbmcplugin.getSetting( "username" )
+        # authenticate user
+        self.authenticate()
         if ( not sys.argv[ 2 ] ):
             self.get_categories()
         else:
             self.get_categories( False )
-
-    def make_presets_folders( self ):
-        if ( not os.path.isdir( xbmc.translatePath( self.BASE_PRESETS_PATH ) ) ):
-            os.makedirs( xbmc.translatePath( self.BASE_PRESETS_PATH ) )
 
     def _parse_argv( self ):
         if ( not sys.argv[ 2 ] ):
@@ -44,27 +36,62 @@ class Main:
             # call _Info() with our formatted argv to create the self.args object
             exec "self.args = _Info(%s)" % ( sys.argv[ 2 ][ 1 : ].replace( "&", ", " ).replace( "\\u0027", "'" ).replace( "\\u0022", '"' ).replace( "\\u0026", "&" ), )
 
+    def authenticate( self ):
+        # if this is first run open settings
+        self.openSettings()
+        # authentication is not permanent, so do this only when first launching plugin
+        if ( not sys.argv[ 2 ] ):
+            # get the users settings
+            password = xbmcplugin.getSetting( "user_password" )
+            # we can only authenticate if both email and password are entered
+            if ( self.username and password ):
+                # our client api
+                from YoutubeAPI.YoutubeClient import YoutubeClient
+                client = YoutubeClient()
+                # make the authentication call
+                authkey, userid = client.authenticate( self.username, password )
+                # if authentication succeeded, save it for later
+                if ( authkey ):
+                    xbmcplugin.setSetting( "authkey", authkey )
+
+    def openSettings( self ):
+        try:
+            # is this the first time plugin was run and user has not set email
+            if ( not sys.argv[ 2 ] and xbmcplugin.getSetting( "username" ) == "" and xbmcplugin.getSetting( "runonce" ) == "" ):
+                # set runonce
+                xbmcplugin.setSetting( "runonce", "1" )
+                # sleep for xbox so dialogs don't clash. (TODO: report this as a bug?)
+                if ( os.environ.get( "OS", "n/a" ) == "xbox" ):
+                    xbmc.sleep( 2000 )
+                # open settings
+                xbmcplugin.openSettings( sys.argv[ 0 ] )
+        except:
+            # new methods not in build
+            pass
+        # we need to get the users email
+        self.username = xbmcplugin.getSetting( "username" )
+
     def get_categories( self, root=True ):
         try:
             # default categories
             if ( root ):
                 categories = (
                                         #( xbmc.getLocalizedString( 30950 ), "all_videos", "", "", True, "updated", 0, "", False, "", ),
-                                        ( xbmc.getLocalizedString( 30951 ), "most_viewed", "", "", True, "viewCount", 0, "", False, 0, ),
-                                        ( xbmc.getLocalizedString( 30952 ), "presets_videos", "", "", True, "updated", 0, "", False, 0, ),
-                                        ( xbmc.getLocalizedString( 30953 ), "presets_users", "", "", True, "updated", 0, "", False, 0, ),
-                                        ( xbmc.getLocalizedString( 30954 ), "recently_featured", "", "", True, "updated", 0, "", False, 0, ),
-                                        ( xbmc.getLocalizedString( 30957 ), "top_rated", "", "", True, "rating", 0, "", False, 0, ),
+                                        ( xbmc.getLocalizedString( 30951 ), "most_viewed", "", "", True, "viewCount", 0, "", False, ),
+                                        ( xbmc.getLocalizedString( 30952 ), "presets_videos", "", "", True, "updated", 0, "", False, ),
+                                        ( xbmc.getLocalizedString( 30953 ), "presets_users", "", "", True, "updated", 0, "", False, ),
+                                        ( xbmc.getLocalizedString( 30954 ), "recently_featured", "", "", True, "updated", 0, "", False, ),
+                                        ( xbmc.getLocalizedString( 30957 ), "top_rated", "", "", True, "rating", 0, "", False, ),
                                         #( xbmc.getLocalizedString( 30958 ), "watch_on_mobile", "", "", True, "updated", 0, "", False, "", ),
                                         #( xbmc.getLocalizedString( 30959 ), "play_random_videos", "", "", False, "relevance", 0, "", False, "", ),
-                                        ( xbmc.getLocalizedString( 30960 ), "play_video_by_id", "", "", False, "relevance", 0, "", False, 0, ),
-                                        ( xbmc.getLocalizedString( 30961 ), "users__uploads", "", "", True, "updated", 0, "", True, 0, ),
-                                        ( xbmc.getLocalizedString( 30962 ), "users__favorites", "", "", True, "updated", 0, "", True, 0, ),
-                                        ( xbmc.getLocalizedString( 30963 ), "top_favorites", "", "", True, "relevance", 0, "", False, 0, ),
-                                        ( xbmc.getLocalizedString( 30964 ), "most_discussed", "", "", True, "relevance", 0, "", False, 0, ),
-                                        ( xbmc.getLocalizedString( 30965 ), "most_linked", "", "", True, "relevance", 0, "", False, 0, ),
-                                        ( xbmc.getLocalizedString( 30966 ), "most_responded", "", "", True, "relevance", 0, "", False, 0, ),
-                                        ( xbmc.getLocalizedString( 30967 ), "most_recent", "", "", True, "relevance", 0, "", False, 0, ),
+                                        ( xbmc.getLocalizedString( 30960 ), "play_video_by_id", "", "", False, "relevance", 0, "", False, ),
+                                        ( xbmc.getLocalizedString( 30961 ), "users__uploads", "", "", True, "updated", 0, "", True, ),
+                                        ( xbmc.getLocalizedString( 30962 ), "users__favorites", "", "", True, "updated", 0, "", True, ),
+                                        ( xbmc.getLocalizedString( 30963 ), "top_favorites", "", "", True, "relevance", 0, "", False, ),
+                                        ( xbmc.getLocalizedString( 30964 ), "most_discussed", "", "", True, "relevance", 0, "", False, ),
+                                        ( xbmc.getLocalizedString( 30965 ), "most_linked", "", "", True, "relevance", 0, "", False, ),
+                                        ( xbmc.getLocalizedString( 30966 ), "most_responded", "", "", True, "relevance", 0, "", False, ),
+                                        ( xbmc.getLocalizedString( 30967 ), "most_recent", "", "", True, "relevance", 0, "", False, ),
                                     )
             # search preset category
             elif ( "category='presets_videos'" in sys.argv[ 2 ] ):
@@ -84,25 +111,19 @@ class Main:
         xbmcplugin.endOfDirectory( handle=int( sys.argv[ 1 ] ), succeeded=ok, cacheToDisc=cacheToDisc )
 
     def get_presets( self, ptype=False ):
-        # we add time to the url, so it is always unique
-        import time
-        # set the proper preset path
-        preset_path = "/".join( [ self.BASE_PRESETS_PATH, ( "videos", "users", )[ ptype ] ] )
+        # set category
+        category = ( "videos", "users", )[ ptype ]
         # initialize our category tuple
         categories = ()
         # add our new search item
         if ( ptype ):
-            categories += ( ( xbmc.getLocalizedString( 30955 ), "search_users", "", "", True, "updated", 1, "", False, time.time(), ), )
+            categories += ( ( xbmc.getLocalizedString( 30955 ), "search_users", "", "", True, "updated", 2, "", False, ), )
         else:
-            categories += ( ( xbmc.getLocalizedString( 30956 ), "search_videos", "", "", True, "updated", 2, "", False, time.time(), ), )
+            categories += ( ( xbmc.getLocalizedString( 30956 ), "search_videos", "", "", True, "updated", 1, "", False, ), )
         # fetch saved presets
         try:
-            # grab a file object
-            fileobject = open( preset_path, "r" )
             # read the queries
-            presets = eval( fileobject.read() )
-            # close file object
-            fileobject.close()
+            presets = eval( xbmcplugin.getSetting( "presets_%s" % ( category, ) ) )
             # sort items
             presets.sort()
         except:
@@ -121,7 +142,7 @@ class Main:
                 else:
                     vq = query.split( " | " )[ 0 ].encode( "utf-8" )
                 # add preset to our dictionary
-                categories += ( ( query.split( " | " )[ 0 ].encode( "utf-8" ), "videos", vq, username, True, "updated", 0, thumbnail, False, 0, ), )
+                categories += ( ( query.split( " | " )[ 0 ].encode( "utf-8" ), "videos", vq, username, True, "updated", 0, thumbnail, False, ), )
             except:
                 # oops print error message
                 print "ERROR: %s::%s (%d) - %s" % ( self.__class__.__name__, sys.exc_info()[ 2 ].tb_frame.f_code.co_name, sys.exc_info()[ 2 ].tb_lineno, sys.exc_info()[ 1 ], )
@@ -131,11 +152,11 @@ class Main:
         try:
             ok = True
             # enumerate through the tuple of categories and add the item to the media list
-            for ( ltitle, method, vq, username, isfolder, orderby, issearch, thumbnail, user_required, time, ) in categories:
+            for ( ltitle, method, vq, username, isfolder, orderby, issearch, thumbnail, user_required, ) in categories:
                 # if a username is required for category and none supplied, skip category
                 if ( user_required and self.username == "" ): continue
                 # set the callback url
-                url = '%s?title=%s&category=%s&page=1&vq=%s&username=%s&orderby=%s&related=""&issearch=%d&update_listing=%d&time=%d' % ( sys.argv[ 0 ], repr( ltitle ), repr( method ), repr( vq ), repr( username ), repr( orderby ), issearch, False, time, )
+                url = '%s?title=%s&category=%s&page=1&vq=%s&username=%s&orderby=%s&related=""&issearch=%d&update_listing=%d' % ( sys.argv[ 0 ], repr( ltitle ), repr( method ), repr( vq ), repr( username ), repr( orderby ), issearch, False, )
                 # check for a valid custom thumbnail for the current category
                 thumbnail = thumbnail or self._get_thumbnail( method )
                 # set the default icon
@@ -165,11 +186,11 @@ class Main:
 
     def _get_thumbnail( self, title ):
         # create the full thumbnail path for skins directory
-        thumbnail = "/".join( [ self.BASE_SKIN_THUMBNAIL_PATH, title.replace( " ", "-" ).lower() + ".png" ] )
+        thumbnail = os.path.join( sys.modules[ "__main__" ].__plugin__, title + ".png" )
         # use a plugin custom thumbnail if a custom skin thumbnail does not exists
-        if ( not os.path.isfile( thumbnail ) ):
+        if ( not xbmc.skinHasImage( thumbnail ) ):
             # create the full thumbnail path for plugin directory
-            thumbnail = os.path.join( self.BASE_PLUGIN_THUMBNAIL_PATH, title.replace( " ", "-" ).lower() + ".png" )
+            thumbnail = os.path.join( self.BASE_PLUGIN_THUMBNAIL_PATH, title + ".png" )
             # use a default thumbnail if a custom thumbnail does not exists
             if ( not os.path.isfile( thumbnail ) ):
                 thumbnail = "DefaultFolderBig.png"
