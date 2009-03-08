ffd6da2ea5e7516bf004b639fa50dd01109a9d2a
BlueCopter <BlueCopter@753fbab9-553a-0410-a42e-33068982fbc4>
2009-03-08 22:29:48 +0000

    [CBS plugin]
    -changed: Episode player update

--- trunk/plugins/video/CBS/resources/lib/episodeplayer.py
+++ trunk/plugins/video/CBS/resources/lib/episodeplayer.py
@@ -14,26 +14,34 @@ class Main:
             name = common.args.name
             pid = common.args.pid
             thumbnail = common.args.thumbnail
-            #Default HD 480p
-            if (xbmcplugin.getSetting("hdquality") == '1'):
-                breakpid = pid.split('<break>')
-                pid=breakpid[0]
-            #Default HD 720p
-            elif (xbmcplugin.getSetting("hdquality") == '2'):
-                breakpid = pid.split('<break>')
-                pid=breakpid[1]
-            #Ask HD Quality
+            #HD Handler
             if '<break>' in pid:
                 breakpid = pid.split('<break>')
-                dia = xbmcgui.Dialog()
-                ret = dia.select('What do you want to do?', ['Play 480p','Play 720p','Exit'])
-                if (ret == 0):
-                        pid=breakpid[0]
-                elif (ret == 1):
-                        pid=breakpid[1]
+                #Default HD 480p
+                if (xbmcplugin.getSetting("hdquality") == '1'):
+                    pid=breakpid[0]
+                    name = '480p: ' + name
+                #Default HD 720p
+                elif (xbmcplugin.getSetting("hdquality") == '2'):
+                    pid=breakpid[1]
+                    name = '720p: ' + name
+                #Ask HD Quality
                 else:
-                        return
-
+                    breakpid = pid.split('<break>')
+                    dia = xbmcgui.Dialog()
+                    ret = dia.select('What do you want to do?', ['Play 480p','Play 720p','Exit'])
+                    #480p selected
+                    if (ret == 0):
+                            pid=breakpid[0]
+                            name = '480p: ' + name
+                    #720p selected
+                    elif (ret == 1):
+                            pid=breakpid[1]
+                            name = '720p: ' + name
+                    #Exit
+                    else:
+                            return
+            #url containing video link
             url = "http://release.theplatform.com/content.select?format=SMIL&Tracking=true&balance=true&pid=" + pid
             link=common.getHTML(url)
             if "rtmp://" in link:
@@ -48,58 +56,13 @@ class Main:
                     stripurl = re.compile('<ref src="http://(.+?)" ').findall(link)
                     finalurl = "http://" + stripurl[0]
                     playpath = ""
-                    name = name + '.flv'
-                    def Download(url,dest):
-                                    dp = xbmcgui.DialogProgress()
-                                    dp.create('Downloading','',name)
-                                    urllib.urlretrieve(url,dest,lambda nb, bs, fs, url=url: _pbhook(nb,bs,fs,url,dp))
-                    def _pbhook(numblocks, blocksize, filesize, url=None,dp=None):
-                                    try:
-                                                    percent = min((numblocks*blocksize*100)/filesize, 100)
-                                                    dp.update(percent)
-                                    except:
-                                                    percent = 100
-                                                    dp.update(percent)
-                                    if dp.iscanceled():
-                                                    dp.close()
-                    flv_file = None
-                    stream = 'false'
-                    if (xbmcplugin.getSetting('download') == '3'):
-                            flv_file = xbmc.translatePath(os.path.join(xbmcplugin.getSetting('download_Path'), name))
-                            Download(finalurl,flv_file)
-                    elif (xbmcplugin.getSetting('download') == '2'):
-                            flv_file = xbmc.translatePath(os.path.join(xbmcplugin.getSetting('download_Path'), name))
-                            Download(finalurl,flv_file)
-                            return
-                    elif (xbmcplugin.getSetting('download') == '0'):
-                            dia = xbmcgui.Dialog()
-                            ret = dia.select('What do you want to do?', ['Play','Download','Download & Play','Exit'])
-                            if (ret == 2):
-                                    flv_file = xbmc.translatePath(os.path.join(xbmcplugin.getSetting('download_Path'), name))
-                                    Download(finalurl,flv_file)
-                            elif (ret == 0):
-                                    stream = 'true'
-                            elif (ret == 1):
-                                    flv_file = xbmc.translatePath(os.path.join(xbmcplugin.getSetting('download_Path'), name))
-                                    Download(finalurl,flv_file)
-                                    return
-                            else:
-                                    return
-                    elif (xbmcplugin.getSetting('download') == '1'):
-                            stream = 'true'
-                    if (flv_file != None and os.path.isfile(flv_file)):
-                            finalurl =str(flv_file)
+                    stream = self.httpDownload(finalurl,name)
+                    if stream == 'false':
+                        return
 
-            if "720p" in finalurl or "720p" in playpath:
-                    finalname = '720p: ' + name
-            elif "480p" in finalurl or "480p" in playpath:
-                    finalname = '480p: ' + name
-            else:
-                    finalname = name
-
-            item=xbmcgui.ListItem(finalname, iconImage=thumbnail, thumbnailImage=thumbnail)
+            item=xbmcgui.ListItem(name, iconImage=thumbnail, thumbnailImage=thumbnail)
             item.setInfo( type="Video",
-                         infoLabels={ "Title": finalname,
+                         infoLabels={ "Title": name,
                                       #"Season": season,
                                       #"Episode": episode,
                                       #"Duration": duration,
@@ -114,3 +77,52 @@ class Main:
             else:
                     player_type = xbmc.PLAYER_CORE_MPLAYER
             ok=xbmc.Player(player_type).play(finalurl, item)
+
+    def httpDownload( self, finalurl, name):
+            name = name + '.flv'
+            def Download(url,dest):
+                            dp = xbmcgui.DialogProgress()
+                            dp.create('Downloading','',name)
+                            urllib.urlretrieve(url,dest,lambda nb, bs, fs, url=url: _pbhook(nb,bs,fs,url,dp))
+            def _pbhook(numblocks, blocksize, filesize, url=None,dp=None):
+                            try:
+                                            percent = min((numblocks*blocksize*100)/filesize, 100)
+                                            dp.update(percent)
+                            except:
+                                            percent = 100
+                                            dp.update(percent)
+                            if dp.iscanceled():
+                                            dp.close()
+            flv_file = None
+            stream = 'false'
+            if (xbmcplugin.getSetting('download') == '0'):
+                    dia = xbmcgui.Dialog()
+                    ret = dia.select('What do you want to do?', ['Play','Download','Download & Play','Exit'])
+                    if (ret == 0):
+                            stream = 'true'
+                    elif (ret == 1):
+                            flv_file = xbmc.translatePath(os.path.join(xbmcplugin.getSetting('download_Path'), name))
+                            Download(finalurl,flv_file)
+                    elif (ret == 2):
+                            flv_file = xbmc.translatePath(os.path.join(xbmcplugin.getSetting('download_Path'), name))
+                            Download(finalurl,flv_file)
+                            stream = 'true'
+                    else:
+                            return stream
+            #Play
+            elif (xbmcplugin.getSetting('download') == '1'):
+                    stream = 'true'
+            #Download
+            elif (xbmcplugin.getSetting('download') == '2'):
+                    flv_file = xbmc.translatePath(os.path.join(xbmcplugin.getSetting('download_Path'), name))
+                    Download(finalurl,flv_file)
+            #Download & Play
+            elif (xbmcplugin.getSetting('download') == '3'):
+                    flv_file = xbmc.translatePath(os.path.join(xbmcplugin.getSetting('download_Path'), name))
+                    Download(finalurl,flv_file)
+                    stream = 'true'            
+            if (flv_file != None and os.path.isfile(flv_file)):
+                    finalurl =str(flv_file)
+            return stream
+        
+        
