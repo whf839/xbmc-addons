d925b411da0dd19253fa7f512be7db23d4b78689
nuka1195@gmail.com <nuka1195@gmail.com@753fbab9-553a-0410-a42e-33068982fbc4>
2009-03-12 02:19:28 +0000

    [Picasa]
    -changed: version -> 1.3.3
    -fixed: unicode issues
    -added: fanart support
    -changed: english language strings

--- trunk/plugins/pictures/Picasa/PicasaAPI/xbmcplugin_categories.py
+++ trunk/plugins/pictures/Picasa/PicasaAPI/xbmcplugin_categories.py
@@ -9,6 +9,8 @@ import xbmc
 import xbmcgui
 import xbmcplugin
 
+from urllib import quote_plus, unquote_plus
+
 
 class _Info:
     def __init__( self, *args, **kwargs ):
@@ -34,7 +36,7 @@ class Main:
             self.args = _Info( title="" )
         else:
             # call _Info() with our formatted argv to create the self.args object
-            exec "self.args = _Info(%s)" % ( sys.argv[ 2 ][ 1 : ].replace( "&", ", " ).replace( "\\u0027", "'" ).replace( "\\u0022", '"' ).replace( "\\u0026", "&" ), )
+            exec "self.args = _Info(%s)" % ( unquote_plus( sys.argv[ 2 ][ 1 : ] ).replace( "&", ", " ).replace( "\\u0027", "'" ).replace( "\\u0022", '"' ).replace( "\\u0026", "&" ), )
 
     def authenticate( self ):
         # if this is first run open settings
@@ -151,7 +153,7 @@ class Main:
                 # if a user id is required for category and none supplied, skip category
                 if ( login_required and self.email == "" ): continue
                 # set the callback url
-                url = '%s?title=%s&category=%s&access=%s&kind=%s&page=1&user_id=%s&album_id=%s&photo_id=%s&pq=%s&issearch=%d&update_listing=%d' % ( sys.argv[ 0 ], repr( ltitle ), repr( method ), repr( access ), repr( kind ), repr( username ), repr( "" ), repr( "" ), repr( pq ), issearch, False, )
+                url = '%s?title=%s&category=%s&access=%s&kind=%s&page=1&user_id=%s&album_id=%s&photo_id=%s&pq=%s&issearch=%d&update_listing=%d' % ( sys.argv[ 0 ], repr( quote_plus( ltitle ) ), repr( method ), repr( access ), repr( kind ), repr( quote_plus( username ) ), repr( "" ), repr( "" ), repr( quote_plus( pq ) ), issearch, False, )
                 # check for a valid custom thumbnail for the current category
                 thumbnail = thumbnail or self._get_thumbnail( method )
                 # set the default icon
@@ -165,14 +167,11 @@ class Main:
             # we do not want to sort queries list
             if ( "category='presets_photos'" in sys.argv[ 2 ] or "category='presets_users'" in sys.argv[ 2 ] ):
                 xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_NONE )
-                try:
-                    # set our plugin category
-                    xbmcplugin.setPluginCategory( handle=int( sys.argv[ 1 ] ), category=self.args.title )
-                    # set our fanart from user setting
-                    #if ( self.settings[ "fanart_image" ] ):
-                    #    xbmcplugin.setPluginFanart( handle=int( sys.argv[ 1 ] ), image=self.settings[ "fanart_image" ], color1=self.settings[ "fanart_color1" ], color2=self.settings[ "fanart_color2" ], color3=self.settings[ "fanart_color3" ] )
-                except:
-                    pass
+            # set our plugin category
+            xbmcplugin.setPluginCategory( handle=int( sys.argv[ 1 ] ), category=self.args.title )
+            # set our fanart from user setting
+            if ( xbmcplugin.getSetting( "fanart_image" ) ):
+                xbmcplugin.setPluginFanart( handle=int( sys.argv[ 1 ] ), image=xbmcplugin.getSetting( "fanart_image" ) )
         except:
             # user cancelled dialog or an error occurred
             print "ERROR: %s::%s (%d) - %s" % ( self.__class__.__name__, sys.exc_info()[ 2 ].tb_frame.f_code.co_name, sys.exc_info()[ 2 ].tb_lineno, sys.exc_info()[ 1 ], )
