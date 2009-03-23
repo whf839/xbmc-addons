9cc73e6216b6b6c3f4069ba9089526d20e210d87
billdaly111 <billdaly111@753fbab9-553a-0410-a42e-33068982fbc4>
2009-03-23 07:23:19 +0000

    Navi-X Media Browser v2.6
    - added: 3-23-2009
    - new: Parental Controls (optional)
    
    Created By:
    Rodejo
    rodejo16@gmail.com

---
+++ scripts/src/CURLLoader.py
@@ -0,0 +1,171 @@
+#############################################################################
+#
+# Navi-X Playlist browser
+# by rodejo (rodejo16@gmail.com)
+#############################################################################
+
+#############################################################################
+#
+# CURLLoader:
+# This class Retrieves the URL to a media item which the XBMC player 
+# understands.
+#############################################################################
+
+from string import *
+import sys, os.path
+import urllib
+import urllib2
+import re, random, string
+import xbmc, xbmcgui
+import re, os, time, datetime, traceback
+import shutil
+import zipfile
+from libs2 import *
+from settings import *
+from CFileLoader import *
+
+try: Emulating = xbmcgui.Emulating
+except: Emulating = False
+
+RootDir = os.getcwd()
+if RootDir[-1]==';': RootDir=RootDir[0:-1]
+if RootDir[-1]!='\\': RootDir=RootDir+'\\'
+imageDir = RootDir + "\\images\\"
+cacheDir = RootDir + "\\cache\\"
+imageCacheDir = RootDir + "\\cache\\imageview\\"
+scriptDir = "Q:\\scripts\\"
+myDownloadsDir = RootDir + "My Downloads\\"
+initDir = RootDir + "\\init\\"
+
+class CURLLoader:
+    ######################################################################
+    # Description: This class is used to retrieve the direct URL of given
+    #              URL which the XBMC player understands.
+    #              
+    # Parameters : URL=source URL
+    # Return     : 0=successful, -1=fail
+    ######################################################################
+    def urlopen(self, URL):
+        result = 0 #successful
+    
+        if URL[:4] == 'http':
+            pos = URL.find("flyupload.com")
+            if pos != -1:
+                result = self.geturl_flyupload(URL)
+            else:
+                pos = URL.rfind('http://') #find last 'http://' in the URL
+                loc_url = URL[pos:]
+                    
+                try:
+                    oldtimeout=socket_getdefaulttimeout()
+                    socket_setdefaulttimeout(url_open_timeout)
+
+                    self.f = urllib.urlopen(loc_url)
+                    self.loc_url=self.f.geturl()
+
+#                    pos = self.loc_url.rfind('http://') #find last 'http' in the URL
+#                    if pos != -1:
+#                        self.loc_url = self.loc_url[pos:]                
+                
+                except IOError:
+                    self.loc_url = "" #could not open URL
+                    socket_setdefaulttimeout(oldtimeout)            
+                    return -1 #fail
+
+                socket_setdefaulttimeout(oldtimeout)
+                
+                #post processing for youtube files
+                pos = URL.find('http://youtube.com') #find last 'http' in the URL
+                if pos != -1:
+                    result = self.geturl_youtube(self.loc_url)
+        else:
+            self.loc_url = URL
+        
+        return result
+
+    ######################################################################
+    # Description: This class is used to retrieve the URL of a FlyUpload
+    #              webpage
+    #              
+    # Parameters : URL=source URL
+    # Return     : 0=successful, -1=fail
+    ######################################################################
+    def geturl_youtube(self, URL):
+        #retrieve the flv file URL
+        #URL parameter is not used.
+#        Trace("voor "+self.loc_url)
+
+        id=''
+        pos = self.loc_url.find('&video_id=') #find last 'http' in the URL
+        if pos != -1:
+            pos2 = self.loc_url.find('&',pos+1) #find last 'http' in the URL
+            id = self.loc_url[pos+10:pos2] #only the video ID
+        
+        try:
+            oldtimeout=socket_getdefaulttimeout()
+            socket_setdefaulttimeout(url_open_timeout)
+
+            #retrieve the timestamp based on the video ID
+            self.f = urllib.urlopen("http://www.youtube.com/api2_rest?method=youtube.videos.get_video_token&video_id=" + id)
+            data=self.f.read()
+            
+            index1 = data.find('<t>')
+            index2 = data.find('</t>')
+            if (index1 != -1) and (index2 != -1):
+                t = data[index1+3:index2]
+                #t contains the timestamp now. Create the URL of the video file (high quality).
+                self.loc_url = "http://www.youtube.com/get_video.php?video_id=" + id + "&amp;t=" + t + "&amp;fmt=18"
+            else:
+                self.loc_url = ""
+        
+        except IOError:
+            self.loc_url = "" #could not open URL
+            return -1 #fail
+        
+        socket_setdefaulttimeout(oldtimeout)
+       
+#        Trace(self.loc_url)        
+        
+        if self.loc_url != "":
+            return 0 #success
+        else:
+            return -1 #failure
+    
+        
+    ######################################################################
+    # Description: This class is used to retrieve the URL of a FlyUpload
+    #              movie
+    #              
+    # Parameters : URL=URL to a FluyUpload webpage
+    # Return     : 0=successful, -1=fail
+    ######################################################################
+    def geturl_flyupload(self, URL):
+        loader = CFileLoader2()
+        loader.load(URL, cacheDir + 'page.html', proxy="DISABLED")
+        if loader.state != 0:
+            return -1 #failure
+        filename = loader.localfile
+        
+        try:
+            f = open(filename, 'r')
+            data = f.read()
+            f.close()
+        except IOError:
+            return -1
+        
+        #defaults
+        result = data.find("thankyou?")
+        if result != -1:
+            result2 = data.find("\"", result)
+            if result2 != -1:
+                self.loc_url = data[result+13:result2]
+            else:
+                return -1 #failure
+        else:
+            return -1 #failure
+
+#        Trace(self.loc_url)
+
+        return 0
+        
+        
---
+++ scripts/src/skin.py
@@ -0,0 +1,152 @@
+#############################################################################
+#
+# Navi-X Playlist browser
+# by rodejo (rodejo16@gmail.com)
+#############################################################################
+
+#############################################################################
+#
+# skin.py:
+# This file loads the GUI elements on the main screen.
+#############################################################################
+
+from string import *
+import sys, os.path
+import urllib
+import urllib2
+import re, random, string
+import xbmc, xbmcgui
+import re, os, time, datetime, traceback
+#import Image, ImageFile
+import shutil
+import zipfile
+import socket
+from settings import *
+
+try: Emulating = xbmcgui.Emulating
+except: Emulating = False
+
+RootDir = os.getcwd()
+if RootDir[-1]==';': RootDir=RootDir[0:-1]
+if RootDir[-1]!='\\': RootDir=RootDir+'\\'
+imageDir = RootDir + "\\images\\"
+cacheDir = RootDir + "\\cache\\"
+imageCacheDir = RootDir + "\\cache\\imageview\\"
+scriptDir = "Q:\\scripts\\"
+myDownloadsDir = RootDir + "My Downloads\\"
+initDir = RootDir + "\\init\\"
+
+######################################################################
+# Description: Draws all widgets on the screen during start-up
+# Parameters : window: handle to the main window
+# Return     : -
+######################################################################
+def load_skin(window):
+    #background image
+    window.bg = xbmcgui.ControlImage(0,0,720,576, imageDir + "background.png")
+    window.addControl(window.bg)
+
+    window.bg1 = xbmcgui.ControlImage(0,0,720,576, imageDir + "background.png")
+    window.addControl(window.bg1)
+    
+    #Navi-X logo at top-right position
+    window.logo = xbmcgui.ControlImage(620,20,80,80, imageDir + "logo.png")
+    window.addControl(window.logo)
+
+    #user logo above the buttons
+    window.user_logo = xbmcgui.ControlImage(32,120,210,172, imageDir + "logo.png")
+    window.addControl(window.user_logo)
+    window.user_logo.setVisible(0)
+            
+    #user thumb above the buttons (overlaps with the user logo)
+    window.user_thumb = xbmcgui.ControlImage(32,120,210,172, imageDir + "logo.png")
+    window.addControl(window.user_thumb)
+    window.user_thumb.setVisible(0)
+    
+    #downloading state at the bottom of screen
+    window.download_logo = xbmcgui.ControlImage(40,480,30,30, imageDir + "icon_download.png")
+    window.addControl(window.download_logo)
+    window.download_logo.setVisible(0)
+    
+    #URL label at top-center position
+    window.urllbl = xbmcgui.ControlLabel(40,80,350,40, "", "font14")
+    window.addControl(window.urllbl)
+
+    #Navi-X Version label at top-left position
+    window.version = xbmcgui.ControlLabel(40,20,155,40, 'version: '+ Version+ '.' + SubVersion, "font14")
+    window.addControl(window.version)      
+    
+    #"Loading..." text at down-left side
+    window.infotekst = xbmcgui.ControlLabel(60,520,250,30, "Loading...")
+    window.addControl(window.infotekst)
+    window.infotekst.setVisible(0)
+    
+    #"Download progress information..." text at down-left side
+    window.dlinfotekst = xbmcgui.ControlLabel(80,490,250,30, "")
+    window.addControl(window.dlinfotekst)
+    window.dlinfotekst.setVisible(0)
+
+    #"Please wait..." text displayed on top of the main list
+    window.loading = xbmcgui.ControlLabel(280, 130, 250, 30, "Please wait...")
+    window.addControl(window.loading)
+    window.loading.setVisible(0)
+    
+    #Main lists displaying playlist entries.
+    #Large list 10 entries without text box on top.
+    window.list1 = xbmcgui.ControlList(260,120,450,435,'font14','0xFFDDDDDD', buttonFocusTexture=RootDir+'images\\list-focus.png', imageWidth=32, imageHeight=32, itemHeight=38)
+    window.addControl(window.list1)
+    window.list1.setPageControlVisible(False)
+
+    #Small list 8 entries with text box on top.
+    window.list2 = xbmcgui.ControlList(260,210,450,345,'font14','0xFFDDDDDD', buttonFocusTexture=RootDir+'images\\list-focus.png', imageWidth=32, imageHeight=32, itemHeight=38)
+    window.addControl(window.list2)
+    window.list2.setPageControlVisible(False)
+
+    #the text box above main list 2
+    window.list2tb = xbmcgui.ControlTextBox(270, 115, 420, 85)
+    window.addControl(window.list2tb)
+    window.list2tb.setVisible(0)
+
+    #set the large list as default
+    window.list = window.list1
+
+    #list cursor position label displayed at the bottom right of the screen
+    window.listpos = xbmcgui.ControlLabel(640,540,250,30, "")
+    window.addControl(window.listpos)
+    
+    #buttons creation
+    x=50
+    y=303
+    window.button_home = xbmcgui.ControlButton(x,y,120,32,'      Home', RootDir + "images\\button-focus1.png", RootDir + "images\\button-nofocus1.png")
+    window.addControl(window.button_home)
+    window.button_favorites = xbmcgui.ControlButton(x,y+32,120,32,'      Favorites', RootDir + "images\\button-focus1.png", RootDir + "images\\button-nofocus1.png")            
+    window.addControl(window.button_favorites)            
+    window.button_downloads = xbmcgui.ControlButton(x,y+64,120,32,'      View', RootDir + "images\\button-focus1.png", RootDir + "images\\button-nofocus1.png")            
+    window.addControl(window.button_downloads)            
+    window.button_url = xbmcgui.ControlButton(x,y+96,120,32,'      Browse', RootDir + "images\\button-focus1.png", RootDir + "images\\button-nofocus1.png")
+    window.addControl(window.button_url)
+    window.button_about = xbmcgui.ControlButton(x,y+128,120,32,'      About', RootDir + "images\\button-focus1.png", RootDir + "images\\button-nofocus1.png")
+    window.addControl(window.button_about)
+
+    #button behaviour
+    window.list2tb.controlDown(window.list1)
+    window.button_home.controlDown(window.button_favorites)
+    window.button_favorites.controlDown(window.button_downloads)
+    window.button_downloads.controlDown(window.button_url)
+    window.button_url.controlDown(window.button_about)
+    window.button_about.controlUp(window.button_url)
+    window.button_url.controlUp(window.button_downloads)
+    window.button_downloads.controlUp(window.button_favorites)
+    window.button_favorites.controlUp(window.button_home)
+    window.button_home.controlUp(window.list2tb)
+
+    #window.button_home.controlRight(window.list1)
+    #window.button_favorites.controlRight(window.list1)
+    #window.button_downloads.controlRight(window.list1)
+    #window.button_url.controlRight(window.list1)
+    #window.button_about.controlRight(window.list1)
+    window.list1.controlLeft(window.button_home)
+    window.list2.controlLeft(window.button_home)
+    window.list2tb.controlLeft(window.button_home)
+  
+
