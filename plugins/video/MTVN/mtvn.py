c5a6e5ee04d5ee5b7e1dcfdae5044878123a5b59
BlueCopter <BlueCopter@753fbab9-553a-0410-a42e-33068982fbc4>
2009-03-25 10:33:46 +0000

    [MTV Networks]
    -removed: feed parser
    -added: element tree for xml parsing
    -added: artists and videos thumbnails

--- plugins/video/MTVN/mtvn.py
+++ plugins/video/MTVN/mtvn.py
@@ -1,11 +1,17 @@
 import urllib, urllib2
 import os, re, sys, md5, string
 import xbmc, xbmcgui, xbmcplugin
-import feedparser
+
+from elementtree.ElementTree import *
+
+media="http://search.yahoo.com/mrss/"
+atom="http://www.w3.org/2005/Atom"
+opensearch="http://a9.com/-/spec/opensearch/1.1/"
+
 
 def getURL( url ):
         try:
-                print 'MTVN --> getHTML :: url = '+url
+                print 'MTVN --> getURL :: url = '+url
                 req = urllib2.Request(url)
                 req.addheaders = [('Referer', 'http://www.mtv.com'),
                                   ('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7)')]
@@ -14,7 +20,18 @@ def getURL( url ):
                 response.close()
         except urllib2.URLError, e:
                 print 'Error code: ', e.code
-                return False
+                try:
+                        print 'MTVN --> getURL ERROR RETRY1 :: url = '+url
+                        req = urllib2.Request(url)
+                        req.addheaders = [('Referer', 'http://www.mtv.com'),
+                                          ('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7)')]
+                        response = urllib2.urlopen(req)
+                        link=response.read()
+                        response.close()
+                except urllib2.URLError, e:
+                        print 'Error code: ', e.code
+                        print 'FAILED TO RETRIVE URL'
+                        return False
         else:
                 return link
 
@@ -44,13 +61,31 @@ def videoSearch(term):
         term = '"'+term+'"'
         encode = urllib.quote_plus(term)
         url = VideoSearch +'?term='+ encode
-        feed = feedparser.parse(url)
+        response = getURL(url)
+        tree1 = ElementTree(fromstring(response))
         videos = []
-        for entry in feed.entries:
+        for entry in tree1.getroot().findall('{%s}entry' % atom):
                 video = []
-                video.append(entry.id)
-                video.append(entry.author + ' - ' +entry.title)
-                video.append('')
+                m = entry.find('{%s}content' % media)
+                video.append(m.get('url').split('/')[-1])
+                name = entry.find('{%s}description' % media).text
+                name = name.split('|')[0] + '-' + name.split('|')[1]
+                video.append(name)
+                turl = [0,'url']
+                for t in entry.findall('{%s}thumbnail' % media):
+                        url = t.get('url')
+                        width = t.get('width')
+                        height = t.get('height')
+                        w = int(width)
+                        h = int(height)
+                        pixels = w*h
+                        if pixels > turl[0]:
+                                turl[0] = pixels
+                                turl[1] = url
+                if turl[1] <> 'url':               
+                        video.append(turl[1])
+                else:
+                        video.append('')
                 videos.append(video)
         return videos
 
@@ -73,13 +108,32 @@ def artistVideos(artist):
         #ArtistVideos = 'http://api.mtvnservices.com/1/artist/[artist_uri]/videos'
         #url = ArtistVideos.replace('[artist_uri]',artist)
         url = artist+'videos/'
-        feed = feedparser.parse(url)
+        response = getURL(url)
+        tree1 = ElementTree(fromstring(response))
         videos = []
-        for entry in feed.entries:
+        count = 0
+        for entry in tree1.getroot().findall('{%s}entry' % atom):
                 video = []
-                video.append(entry.id)
-                video.append(entry.title)
-                video.append('')
+                m = entry.find('{%s}content' % media)
+                video.append(m.get('url').split('/')[-1])
+                name = entry.find('{%s}description' % media).text
+                name = name.split('|')[1]
+                video.append(name)
+                turl = [0,'url']
+                for t in entry.findall('{%s}thumbnail' % media):
+                        url = t.get('url')
+                        width = t.get('width')
+                        height = t.get('height')
+                        w = int(width)
+                        h = int(height)
+                        pixels = w*h
+                        if pixels > turl[0]:
+                                turl[0] = pixels
+                                turl[1] = url
+                if turl[1] <> 'url':               
+                        video.append(turl[1])
+                else:
+                        video.append('')
                 videos.append(video)
         return videos
 
@@ -91,14 +145,28 @@ def artistSearch(term):
         encode = urllib.quote_plus(term)
         url = ArtistSearch +'?term='+ encode
         print url
-        feed = feedparser.parse(url)
-        print feed
+        response = getURL(url)
+        tree1 = ElementTree(fromstring(response))
         artists = []
-        for entry in feed.entries:
+        for entry in tree1.getroot().findall('{%s}entry' % atom):
                 artist = []
-                artist.append(entry.id)
-                artist.append(entry.title)
-                artist.append('')
+                artist.append(entry.find('{%s}id' % atom).text)
+                artist.append(entry.find('{%s}title' % atom).text)
+                turl = [0,'url']
+                for t in entry.findall('{%s}thumbnail' % media):
+                        url = t.get('url')
+                        width = t.get('width')
+                        height = t.get('height')
+                        w = int(width)
+                        h = int(height)
+                        pixels = w*h
+                        if pixels > turl[0]:
+                                turl[0] = pixels
+                                turl[1] = url
+                if turl[1] <> 'url':               
+                        artist.append(turl[1])
+                else:
+                        artist.append('')
                 artists.append(artist)
         return artists
 
@@ -107,16 +175,30 @@ def artistSearch(term):
 def artistBrowse(letter):
         ArtistBrowse = 'http://api.mtvnservices.com/1/artist/browse/[alpha]'
         url = ArtistBrowse.replace('[alpha]',letter)
-        feed = feedparser.parse(url)
+        response = getURL(url)
+        tree1 = ElementTree(fromstring(response))
         artists = []
-        for entry in feed.entries:
+        for entry in tree1.getroot().findall('{%s}entry' % atom):
                 artist = []
-                artist.append(entry.id)
-                artist.append(entry.title)
-                artist.append('')
+                artist.append(entry.find('{%s}id' % atom).text)
+                artist.append(entry.find('{%s}title' % atom).text)
+                turl = [0,'url']
+                for t in entry.findall('{%s}thumbnail' % media):
+                        url = t.get('url')
+                        width = t.get('width')
+                        height = t.get('height')
+                        w = int(width)
+                        h = int(height)
+                        pixels = w*h
+                        if pixels > turl[0]:
+                                turl[0] = pixels
+                                turl[1] = url
+                if turl[1] <> 'url':               
+                        artist.append(turl[1])
+                else:
+                        artist.append('')
                 artists.append(artist)
         return artists
-
 #Returns a list of artists related to the specified artist.
 def relatedArtist(artist):
         RelatedArtist = 'http://api.mtvnservices.com/1/artist/[artist_uri]/related/'
@@ -132,15 +214,30 @@ def genreAlias(genre):
 
 #Returns a list of artists for a specified genre.
 def genreArtists(genre):
-        GenreArtists = 'http://api.mtvnservices.com/1/genre/[genre_alias]/artists/?max-results=100'
+        GenreArtists = 'http://api.mtvnservices.com/1/genre/[genre_alias]/artists/'
         url = GenreArtists.replace('[genre_alias]',genre)
-        feed = feedparser.parse(url)
+        response = getURL(url)
+        tree1 = ElementTree(fromstring(response))
         artists = []
-        for entry in feed.entries:
+        for entry in tree1.getroot().findall('{%s}entry' % atom):
                 artist = []
-                artist.append(entry.id)
-                artist.append(entry.title)
-                artist.append('')
+                artist.append(entry.find('{%s}id' % atom).text)
+                artist.append(entry.find('{%s}title' % atom).text)
+                turl = [0,'url']
+                for t in entry.findall('{%s}thumbnail' % media):
+                        url = t.get('url')
+                        width = t.get('width')
+                        height = t.get('height')
+                        w = int(width)
+                        h = int(height)
+                        pixels = w*h
+                        if pixels > turl[0]:
+                                turl[0] = pixels
+                                turl[1] = url
+                if turl[1] <> 'url':               
+                        artist.append(turl[1])
+                else:
+                        artist.append('')
                 artists.append(artist)
         return artists
 
@@ -148,13 +245,31 @@ def genreArtists(genre):
 def genreVideos(genre):
         GenreVideos = 'http://api.mtvnservices.com/1/genre/[genre_alias]/videos/'
         url = GenreVideos.replace('[genre_alias]',genre)
-        feed = feedparser.parse(url)
+        response = getURL(url)
+        tree1 = ElementTree(fromstring(response))
         videos = []
-        for entry in feed.entries:
+        for entry in tree1.getroot().findall('{%s}entry' % atom):
                 video = []
-                video.append(entry.id)
-                video.append(entry.author + ' - ' +entry.title)
-                video.append('')
+                m = entry.find('{%s}content' % media)
+                video.append(m.get('url').split('/')[-1])
+                name = entry.find('{%s}description' % media).text
+                name = name.split('|')[0] + '-' + name.split('|')[1]
+                video.append(name)
+                turl = [0,'url']
+                for t in entry.findall('{%s}thumbnail' % media):
+                        url = t.get('url')
+                        width = t.get('width')
+                        height = t.get('height')
+                        w = int(width)
+                        h = int(height)
+                        pixels = w*h
+                        if pixels > turl[0]:
+                                turl[0] = pixels
+                                turl[1] = url
+                if turl[1] <> 'url':               
+                        video.append(turl[1])
+                else:
+                        video.append('')
                 videos.append(video)
         return videos
 
@@ -162,10 +277,7 @@ def genreVideos(genre):
 ####################################      RTMP      ####################################       
 ########################################################################################
 
-def getrtmp(url):
-        uridata = getURL(url)
-        uri = re.compile('content url="(.+?)"').findall(uridata)[0]
-        uri = uri.replace('http://media.mtvnservices.com/','')
+def getrtmp(uri):
         print uri
         smilurl = 'http://api-media.mtvnservices.com/player/embed/includes/mediaGen.jhtml?uri='+uri+'&vid='+uri.split(':')[4]+'&ref={ref}'
         smil = getURL(smilurl)
