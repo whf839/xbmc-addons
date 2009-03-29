76c2635ae3c0624af752e06bb9a15d65ae4bf404
nuka1195 <nuka1195@753fbab9-553a-0410-a42e-33068982fbc4>
2009-03-29 20:07:19 +0000

    [CNN Video]
    -changed: version -> 1.2.2
    -changed: use new larger thumbnails

--- trunk/plugins/video/CNN Video/default.py
+++ trunk/plugins/video/CNN Video/default.py
@@ -12,7 +12,7 @@ __author__ = "nuka1195"
 __url__ = "http://code.google.com/p/xbmc-addons/"
 __svn_url__ = "http://xbmc-addons.googlecode.com/svn/trunk/plugins/video/CNN%20Video"
 __credits__ = "Team XBMC"
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 xbmc.log( "[PLUGIN] '%s: version %s' initialized!" % ( __plugin__, __version__, ), xbmc.LOGNOTICE )
 
