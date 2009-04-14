814122a853c96c40e0548036ce401245a45d044f
nuka1195@gmail.com <nuka1195@gmail.com@753fbab9-553a-0410-a42e-33068982fbc4>
2009-04-14 21:50:47 +0000

    [SVN Repo Installer]
    -changed: version -> 1.5.4
    -added: check for compatibility at start
    -changed: English language strings
    -changed: some code cleanup, thanks BBB

--- plugins/programs/SVN Repo Installer/installerAPI/xbmcplugin_update.py
+++ plugins/programs/SVN Repo Installer/installerAPI/xbmcplugin_update.py
@@ -35,11 +35,6 @@ if not os.path.isdir(xbmc.translatePath(XBMC_HOME)):    # if fails to convert to
     XBMC_HOME = 'Q:'
 log("XBMC_HOME=%s" % XBMC_HOME)
 
-class _Info:
-    def __init__( self, *args, **kwargs ):
-        self.__dict__.update( kwargs )
-
-#############################################################################################################
 class Main:
 
     URL_BASE_SVN_SCRIPTING = "http://xbmc-scripting.googlecode.com/svn"
@@ -49,15 +44,12 @@ class Main:
         log("__init__()")
         ok = False
 
-        # parse sys.argv for our current url
-        self._parse_argv()
-
         # load settings
         self.showNoSVN = bool(xbmcplugin.getSetting( "show_no_svn" ) == "true")
         self.showNoVer = bool(xbmcplugin.getSetting( "show_no_ver" ) == "true")
         
-        #print xbmc.getInfoLabel( "System.BuildVersion" )
-        self.XBMC_REVISON = re.search("r([0-9]+)",  xbmc.getInfoLabel( "System.BuildVersion" ), re.IGNORECASE).group(1)
+        self.XBMC_REVISION = re.search("r([0-9]+)",  xbmc.getInfoLabel( "System.BuildVersion" ), re.IGNORECASE).group(1)
+        log("XBMC_REVISION-%s" % self.XBMC_REVISION)
         self.SVN_URL_LIST = [ "xbmc-addons", "xbmc-scripting" ]
         #['plugin://programs/SVN Repo Installer/', '-1', '?download_url="%2Ftrunk%2Fplugins%2Fmusic/iTunes%2F"&repo=\'xbmc-addons\'&install=""&ioffset=2&voffset=0']
         # create all XBMC script/plugin paths
@@ -78,13 +70,6 @@ class Main:
 
         xbmcplugin.endOfDirectory( int( sys.argv[ 1 ] ), ok)
 
-    def _parse_argv( self ):
-        # call _Info() with our formatted argv to create the self.args object
-        cmd = "self.args = _Info(%s)" % ( urllib.unquote_plus( sys.argv[ 2 ][ 1 : ].replace( "&", ", " ) ), )
-        log("_parse_argv() cmd=" + cmd)
-        exec cmd
-
-    #####################################################################################################
     def findInstalled(self):
         log("> findInstalled()")
         
@@ -115,7 +100,7 @@ class Main:
                     if ver or self.showNoVer:
                         self.INSTALLED.append({"filepath": filepath, "ver": ver, "thumb": thumb, "xbmc_rev": xbmc_rev })
                 except:
-                    traceback.print_exc()
+                    pass#traceback.print_exc()
 
 #        pprint (self.INSTALLED)
         log("< findInstalled()  installed count=%d" % len(self.INSTALLED))
@@ -139,7 +124,6 @@ class Main:
 
             for repo in self.SVN_URL_LIST:
                 repo_info = self._get_repo_info( repo )
-#                print repo_info
                 base_url = repo_info[ 0 ] + repo_info[ 1 ]
                 # xbmc-scripting has no 'scripts' in svn path, so remove it from installedCategory
                 if base_url.startswith(self.URL_BASE_SVN_SCRIPTING):
@@ -167,10 +151,9 @@ class Main:
                 else:
                     # check __version__ tag
                     svn_ver = self.parseVersion(doc)
-                    svn_xbmc_rev = self.parseXBMCRevision(doc)
                     if svn_ver:
                         self.INSTALLED[count]['svn_ver'] = svn_ver
-                        self.INSTALLED[count]['svn_xbmc_rev'] = svn_xbmc_rev
+                        self.INSTALLED[count]['svn_xbmc_rev'] = self.parseXBMCRevision(doc)
                         self.INSTALLED[count]['svn_url'] = url.replace('/default.py','')
                         self.INSTALLED[count]['repo'] = repo
                         self.INSTALLED[count]['install'] = repo_info[ 2 ][ 2 ]
@@ -181,7 +164,6 @@ class Main:
 
             if quit: break
 
-#        pprint (self.INSTALLED)
         log("< checkUpdates() updated count=%d" % len(self.INSTALLED))
 
     #####################################################################################################
@@ -238,13 +220,12 @@ class Main:
     def showUpdates(self):
         log("> showUpdates()")
         ok = False
-        log("showUpdates() sys.argv[ 0 ]=" + sys.argv[ 0 ])
+        log("showUpdates()")
 
         try:
             # create display list
             sz = len(self.INSTALLED)
             for info in self.INSTALLED:
-#                print info
                 path = ""
                 ver = info.get('ver', '')
                 rev = info.get('xbmc_rev', '')
@@ -267,13 +248,9 @@ class Main:
                     verState = xbmc.getLocalizedString( 30012 )         # not in SVN
                 elif ver >= svn_ver:
                     verState = xbmc.getLocalizedString( 30011 )            # OK
-                    path = os.path.join(filepath, 'default.tbn')        # so we have a unique path still
-                else:
-                    if (svn_xbmc_rev and self.XBMC_REVISON and self.XBMC_REVISON >= svn_xbmc_rev) or (not svn_xbmc_rev or not self.XBMC_REVISON):
-                        # NEW AVAILABLE - setup callback url for plugin SVN Repo Installer
-                        verState = "!%s! v%s" % ( xbmc.getLocalizedString( 30014 ), svn_ver )        # eg. !New! v1.0
-                    else:
-                        verState = "*%s* v%s" % ( xbmc.getLocalizedString( 30015 ), svn_ver )        # eg. !New! v1.0
+                elif (svn_xbmc_rev and self.XBMC_REVISION and self.XBMC_REVISION >= svn_xbmc_rev) or (not svn_xbmc_rev or not self.XBMC_REVISION):
+                    # NEW AVAILABLE - setup callback url for plugin SVN Repo Installer
+                    verState = "v%s (%s)" % ( svn_ver, xbmc.getLocalizedString( 30014 ) )        # eg. !New! v1.0
                     trunk_url = re.search('(/trunk.*?)$', svn_url, re.IGNORECASE).group(1)
                     #['plugin://programs/SVN Repo Installer/', '-1', '?download_url="%2Ftrunk%2Fplugins%2Fmusic/iTunes%2F"&repo=\'xbmc-addons\'&install=""&ioffset=2&voffset=0']
                     url_args = "download_url=%s&repo=%s&install=%s&ioffset=%s&voffset=%s" % \
@@ -283,15 +260,19 @@ class Main:
                                 info["ioffset"],
                                 info["voffset"],)
                     path = '%s?%s' % ( sys.argv[ 0 ], url_args, )
+                else:
+                    verState = "v%s (%s)" % ( svn_ver, xbmc.getLocalizedString( 30015 ), )        # eg. Incompatible
 
+                if not path:
+                    path = os.path.join(filepath, 'default.tbn')
                 log("path=" + path)
 
                 # display text
                 text = "[%s] %s v%s" % (repo, category, ver)
-                if verState.startswith( "!" ):                            # !New! - checking against ! as its constant regardless of lang
+                if verState.endswith( "(%s)" % xbmc.getLocalizedString( 30014 ) ):                            # !New! - checking against ! as its constant regardless of lang
                     label2 = "[COLOR=FFFF0000]%s[/COLOR]" % verState
-                elif verState.startswith( "*" ):
-                    label2 = "[COLOR=FFFF9900]%s[/COLOR]" % verState
+                elif verState.endswith( "(%s)" % xbmc.getLocalizedString( 30015 ) ):
+                    label2 = "[COLOR=FF00FFFF]%s[/COLOR]" % verState
                 elif verState == "OK":
                     label2 = "[COLOR=FF00FF00]%s[/COLOR]" % verState
                 else:
@@ -336,7 +317,7 @@ class Main:
         log("< showUpdates() ok=%s" % ok)
         return ok
     
-#################################################################################################################
+
 if ( __name__ == "__main__" ):
     try:
         Main()
