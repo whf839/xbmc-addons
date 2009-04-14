15988b3b211b5888a0280c67e478a4f5c0720d91
nuka1195@gmail.com <nuka1195@gmail.com@753fbab9-553a-0410-a42e-33068982fbc4>
2009-04-14 21:50:47 +0000

    [SVN Repo Installer]
    -changed: version -> 1.5.4
    -added: check for compatibility at start
    -changed: English language strings
    -changed: some code cleanup, thanks BBB

--- trunk/plugins/programs/SVN Repo Installer/default.py
+++ trunk/plugins/programs/SVN Repo Installer/default.py
@@ -12,18 +12,46 @@ __author__ = "nuka1195/BigBellyBilly"
 __url__ = "http://code.google.com/p/xbmc-addons/"
 __svn_url__ = "http://xbmc-addons.googlecode.com/svn/trunk/plugins/programs/SVN%20Repo%20Installer"
 __credits__ = "Team XBMC"
-__version__ = "1.5.3"
+__version__ = "1.5.4"
 __XBMC_Revision__ = "19001"
 
 
-xbmc.log( "[PLUGIN] '%s: version %s' initialized!" % ( __plugin__, __version__, ), xbmc.LOGNOTICE )
-
+def _check_compatible():
+    try:
+        # spam plugin statistics to log
+        xbmc.log( "[PLUGIN] '%s: version %s' initialized!" % ( __plugin__, __version__, ), xbmc.LOGNOTICE )
+        # get xbmc revision
+        xbmc_version = xbmc.getInfoLabel( "System.BuildVersion" )
+        xbmc_rev = 0
+        xbmc_rev = int( xbmc_version.split( " " )[ 1 ].replace( "r", "" ) )
+        # compatible?
+        ok = xbmc_rev >= int( __XBMC_Revision__ )
+    except:
+        # error, so make incompatible
+        ok = False
+    # spam revision info
+    xbmc.log( "     ** Required XBMC Revision: r%s **" % ( __XBMC_Revision__, ), xbmc.LOGNOTICE )
+    xbmc.log( "     ** Found XBMC Revision: r%d [%s] **" % ( xbmc_rev, ( "Not Compatible", "Compatible", )[ ok ], ), xbmc.LOGNOTICE )
+    # if not compatible, inform user
+    if ( not ok ):
+        import xbmcgui
+        xbmcgui.Dialog().ok( "%s - %s: %s" % ( __plugin__, xbmc.getLocalizedString( 30700 ), __version__, ), xbmc.getLocalizedString( 30701 ) % ( __plugin__, ), xbmc.getLocalizedString( 30702 ) % ( __XBMC_Revision__, ), xbmc.getLocalizedString( 30703 ) )
+    #return result
+    return ok
+    
 
 if ( __name__ == "__main__" ):
-    if ( "download_url=" in sys.argv[ 2 ] ):
+    if ( not sys.argv[ 2 ] ):
+        # check for compatibility, only need to check this once
+        ok = _check_compatible()
+        # only run if ok
+        if ( ok ):
+            from installerAPI import xbmcplugin_list as plugin
+    elif ( "download_url=" in sys.argv[ 2 ] ):
         from installerAPI import xbmcplugin_downloader as plugin
     elif ( sys.argv[ 2 ] == "?category='updates'" ):
         from installerAPI import xbmcplugin_update as plugin
-    else:
-        from installerAPI import xbmcplugin_list as plugin
-    plugin.Main()
+    try:
+        plugin.Main()
+    except:
+        pass
