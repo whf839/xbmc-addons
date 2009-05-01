4d4bdf0a3c3b8663df73a8318fc665f149141b8a
billdaly111 <billdaly111@753fbab9-553a-0410-a42e-33068982fbc4>
2009-05-01 21:21:03 +0000

    Navi-X Media Browser v2.7 (PLUGIN) Not the script!
    Now compatible with Boxee and AppleTV!
    Check out www.navi-x.org for more details!

---
+++ trunk/plugins/video/Navi-X Plugin/src/CFileLoader.py
@@ -0,0 +1,315 @@
+#############################################################################
+#
+# Navi-X Playlist browser
+# by rodejo (rodejo16@gmail.com)
+#############################################################################
+
+#############################################################################
+#
+# CFileloader:
+# This class is a generic file loader and handles downloading a file to disk.
+#############################################################################
+
+from string import *
+import sys, os.path
+import urllib
+import urllib2
+import re, random, string
+import xbmc, xbmcgui
+import re, os, time, datetime, traceback
+import shutil
+import zipfile
+from settings import *
+from libs2 import *
+
+try: Emulating = xbmcgui.Emulating
+except: Emulating = False
+
+#RootDir = os.getcwd()
+#if RootDir[-1]==';': RootDir=RootDir[0:-1]
+#if RootDir[-1]!='\\': RootDir=RootDir+'\\'
+#imageDir = RootDir + "\\images\\"
+#cacheDir = RootDir + "\\cache\\"
+#imageCacheDir = RootDir + "\\cache\\imageview\\"
+#scriptDir = "Q:\\scripts\\"
+#myDownloadsDir = RootDir + "My Downloads\\"
+#initDir = RootDir + "\\init\\"
+
+######################################################################
+# Description: Downloads a URL to local disk. 
+######################################################################
+#class CFileLoader:
+#    ######################################################################
+#    # Description: Downloads a file in case of URL and returns absolute
+#    #              path to the local file.
+#    # Parameters : URL=source, localfile=destination
+#    # Return     : -
+#    ######################################################################
+#    def load(self, URL, localfile):
+#        if URL[:4] == 'http':
+#            try:
+#                loc = urllib.URLopener()
+#                loc.retrieve(URL, localfile)
+#            except IOError:
+#                self.state = -1 #failed
+#                return
+#            self.state = 0 #success
+#            self.localfile = localfile
+#        elif URL[1] == ':': #absolute path
+#            self.localfile = URL
+#            self.state = 0 #success
+#        else: #assuming relative path
+#            self.localfile = RootDir + '\\' + URL
+#            self.state = 0 #success 
+
+#class CFileLoader3:
+#    ######################################################################
+#    # Description: Downloads a file in case of URL and returns absolute
+#    #              path to the local file.
+#    # Parameters : URL=source
+#    #              localfile=destination
+#    #              timeout(optional)=Funtion time out time.
+#    # Return     : -
+#    ######################################################################
+#    def load(self, URL, localfile, timout=url_open_timeout):
+#        if URL[:4] == 'http':
+#            try:
+#                oldtimeout=socket.getdefaulttimeout()
+#                socket.setdefaulttimeout(timout)
+#            
+#                f = urllib.urlopen(URL)
+#                #get the size of the file in bytes
+##                size_string=f.info().getheader("Content-Length")
+#
+#                #open the destination file
+#                file = open(localfile, "wb")
+#                #file.write(f.read(int(size_string)))
+#                file.write(f.read())
+#                file.close()          
+#                  
+#            except IOError:
+#                socket.setdefaulttimeout(oldtimeout)            
+#                self.state = -1 #failed
+#                return
+#
+#            socket.setdefaulttimeout(oldtimeout)
+#                
+#            self.localfile = localfile
+#            self.state = 0 #success
+#            
+#        elif URL[1] == ':': #absolute path
+#            self.localfile = URL
+#            self.state = 0 #success
+#        else: #assuming relative path
+#            self.localfile = RootDir + '\\' + URL
+#            self.state = 0 #success 
+
+#class CFileLoader2:
+#    ######################################################################
+#    # Description: Downloads a file in case of URL and returns absolute
+#    #              path to the local file.
+#    # Parameters : URL=source, localfile=destination
+#    # Return     : -
+#    ######################################################################
+#    def load(self, URL, localfile):
+#        if URL[:4] == 'http':
+#            try:
+#                values = { 'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+#                req = urllib2.Request(URL, None, values)
+#                f = urllib2.urlopen(req)
+#                
+#                #open the destination file
+#                file = open(localfile, "wb")
+#                #file.write(f.read(int(size_string)))
+#                file.write(f.read())
+#                file.close()          
+#                  
+#            except IOError:
+#                self.state = -1 #failed
+#                return
+#                
+#            self.localfile = localfile
+#            self.state = 0 #success
+#            
+#        elif URL[1] == ':': #absolute path
+#            self.localfile = URL
+#            self.state = 0 #success
+#        else: #assuming relative path
+#            self.localfile = RootDir + '\\' + URL
+#            self.state = 0 #success 
+
+class CFileLoader:
+    ######################################################################
+    # Description: Downloads a file in case of URL and returns absolute
+    #              path to the local file.
+    # Parameters : URL=source
+    #              localfile=destination
+    #              timeout(optional)=Funtion time out time.
+    # Return     : -
+    ######################################################################
+    def load(self, URL, localfile, timeout=url_open_timeout, proxy="CACHING", content_type= ''):
+        if (URL == ''): #or (localfile == ''):
+            self.state = -1 #failed
+            return
+        
+        if URL[:4] == 'http':
+            sum_str = ''
+            if proxy != "DISABLED":
+                sum = 0
+                #calculate hash of URL
+                for i in range(len(URL)):
+                    sum = sum + (ord(URL[i]) * i)
+                sum_str = str(sum)
+
+            if localfile != '':
+                ext_pos = localfile.rfind('.') #find last '.' in the string
+                if ext_pos != -1:
+                    localfile = localfile[:ext_pos] + sum_str + localfile[ext_pos:]
+                else:
+                    localfile = localfile + sum_str
+
+            if (not((proxy == "ENABLED") and (os.path.exists(localfile) == True))): 
+                try:
+                    oldtimeout=socket_getdefaulttimeout()
+                    socket_setdefaulttimeout(timeout)
+            
+                    f = urllib.urlopen(URL)
+
+                    headers = f.info()
+                    type = headers['Content-Type']
+                    type = type.lower()
+                    
+#                    Trace("Type " + type)
+                    
+                    if (content_type != '') and (type.find(content_type)  == -1):
+                        #unexpected type
+                        socket_setdefaulttimeout(oldtimeout)            
+                        self.state = -2 #failed
+                        return
+
+                    self.data = f.read()
+                                      
+                    if localfile != '':
+                        #open the destination file
+                        file = open(localfile, "wb")
+                        file.write(self.data)
+                        file.close()          
+                  
+                except IOError:
+                    socket_setdefaulttimeout(oldtimeout)            
+                    self.state = -1 #failed
+                    return
+
+                socket_setdefaulttimeout(oldtimeout)
+                
+            self.localfile = localfile
+            self.state = 0 #success
+            
+        elif URL[1] == ':': #absolute (local) path
+            self.localfile = URL
+            self.state = 0 #success
+            try:
+                f = open(self.localfile, "r")
+                self.data = f.read()
+                f.close()
+            except IOError:
+                self.state = -2
+        else: #assuming relative (local) path
+            self.localfile = RootDir + URL        
+            self.state = 0 #success
+            try:
+                f = open(self.localfile, "r")
+                self.data = f.read()
+                f.close()
+            except IOError:
+                self.state = -2
+
+
+
+class CFileLoader2:
+    ######################################################################
+    # Description: Downloads a file in case of URL and returns absolute
+    #              path to the local file.
+    # Parameters : URL=source, localfile=destination
+    # Return     : -
+    ######################################################################
+    def load(self, URL, localfile, timeout=url_open_timeout, proxy="CACHING", content_type= ''):
+        if (URL == '') or (localfile == ''):
+            self.state = -1 #failed
+            return
+        
+        if URL[:4] == 'http':
+            sum_str = ''
+            if proxy != "DISABLED":
+                sum = 0
+                #calculate hash of URL
+                for i in range(len(URL)):
+                    sum = sum + (ord(URL[i]) * i)
+                sum_str = str(sum)
+
+            ext_pos = localfile.rfind('.') #find last '.' in the string
+            if ext_pos != -1:
+                localfile = localfile[:ext_pos] + sum_str + localfile[ext_pos:]
+            else:
+                localfile = localfile + sum_str
+
+            if (not((proxy == "ENABLED") and (os.path.exists(localfile) == True))): 
+                try:
+                    oldtimeout=socket_getdefaulttimeout()
+                    socket_setdefaulttimeout(timeout)
+            
+                    values = { 'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+                    #req = urllib2.Request(URL, None, values)
+                    req = urllib2.Request(URL)
+                    f = urllib2.urlopen(req)
+                
+                    headers = f.info()
+                    type = headers['Content-Type']
+                    
+                    if (content_type != '') and (type.find(content_type)  == -1):
+                        #unexpected type
+                        socket_setdefaulttimeout(oldtimeout)            
+                        self.state = -1 #failed
+                        return
+                
+                    #open the destination file
+                    file = open(localfile, "wb")
+                    #file.write(f.read(int(size_string)))
+                    file.write(f.read())
+                    file.close()          
+                  
+                except IOError:
+                    socket_setdefaulttimeout(oldtimeout)            
+
+#                    Trace("You have an IOError: ")
+                    self.state = -1 #failed
+                    return
+
+#                except urllib2.HTTPError:
+#                    socket_setdefaulttimeout(oldtimeout)
+#
+#                    Trace("There was an http error: ")
+#                    self.state = -1 #failed
+#                    return
+
+#                except urllib2.URLError, e:
+#                    socket_setdefaulttimeout(oldtimeout)
+#
+#                    Trace("There is a problem with the URL: " + str(e.reason))
+#                    self.state = -1 #failed
+#                    return
+
+                socket_setdefaulttimeout(oldtimeout)
+                
+            self.localfile = localfile
+            self.state = 0 #success
+            
+        elif (URL[1] == ':') or (URL[0] == '/'): #absolute (local) path
+            self.localfile = URL
+            self.state = 0 #success
+        else: #assuming relative (local) path
+            self.localfile = RootDir + '\\' + URL
+            self.state = 0 #success
+           
+
+        
