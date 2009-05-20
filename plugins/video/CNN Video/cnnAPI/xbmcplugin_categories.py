f380d3c5bb6aa9317860c757e9295461e4addd9d
nuka1195 <nuka1195@753fbab9-553a-0410-a42e-33068982fbc4>
2009-05-20 15:04:17 +0000

    [CNN Video]
    -changed: version -> 1.3
    -fixed: category list did not show(hard coded categories)
    -added: english language strings
    -added: setting to use large thumbnail(off by default)
    -changed: use category for thumbnail not title
    -changed: renamed world thumbnail to match new category

--- plugins/video/CNN Video/cnnAPI/xbmcplugin_categories.py
+++ plugins/video/CNN Video/cnnAPI/xbmcplugin_categories.py
@@ -9,7 +9,6 @@ import xbmc
 import xbmcgui
 import xbmcplugin
 
-from sgmllib import SGMLParser
 import urllib
 
 
@@ -18,75 +17,86 @@ class _Info:
         self.__dict__.update( kwargs )
 
 
-class _Parser( SGMLParser ):
-    """
-        Parses an html document for categories
-    """
-    # base urls
-    BASE_URL = "http://www.cnn.com/"
-
-    def reset( self ):
-        SGMLParser.reset( self )
-        # list of _Info() objects
-        self.categories = []
-        self._init_vars()
-
-    def _init_vars( self ):
-        # reset content variables
-        self.title = []
-        self.category = ""
-
-    def start_a( self, attrs ):
-        # links are in href keys
-        for key, value in attrs:
-            # if it is a valid category set it
-            if ( key == "href" and value.startswith( "javascript:load" ) and "," in value ):
-                self.category = value.split( "," )[ 1 ].strip().replace( "'", "" ).replace( ";", "" ).replace( ")", "" )[ 1 : ]
-
-    def end_a( self ):
-        # if we found a valid category add it to our list and reset content variables
-        if ( self.category ):
-            self.make_object( " ".join( self.title ), self.category )
-            self._init_vars()
-
-    def handle_data( self, text ):
-        # if we found a valid category grab the category title
-        if ( self.category ):
-            self.title += [ text.title().replace( "Cnn", "CNN" ).replace( "Tv", "TV" ) ]
-
-    def make_object( self, title, category ):
-        # add item to our _Info() object list
-        self.categories += [ _Info( title=title, url=self.BASE_URL + category ) ]
-
-
 class Main:
     # base urls
-    BASE_CATEGORY_URL = "http://www.cnn.com/video"
+    BASE_URL = "http://www.cnn.com/.element/ssi/www/auto/2.0/video/xml/%s.xml"
 
     # base paths
     BASE_PLUGIN_THUMBNAIL_PATH = os.path.join( os.getcwd(), "thumbnails" )
 
     def __init__( self ):
+        self._parse_argv()
         self.get_categories()
+
+    def _parse_argv( self ):
+        if ( not sys.argv[ 2 ] ):
+            self.args = _Info( title="" )
+        else:
+            # call _Info() with our formatted argv to create the self.args object
+            exec "self.args = _Info(%s)" % ( urllib.unquote_plus( sys.argv[ 2 ][ 1 : ].replace( "&", ", " ) ), )
 
     def get_categories( self ):
         try:
-            # open url
-            usock = urllib.urlopen( self.BASE_CATEGORY_URL )
-            # read source
-            htmlSource = usock.read()
-            # close socket
-            usock.close()
-            # Parse htmlSource for categories
-            parser = _Parser()
-            parser.feed( htmlSource )
-            parser.close()
+            if ( not sys.argv[ 2 ] ):
+                categories = (
+                                        ( xbmc.getLocalizedString( 30900 ), "categories", ),
+                                        ( xbmc.getLocalizedString( 30901 ), "ireports", ),
+                                        #( xbmc.getLocalizedString( 30902 ), "ontv", ),
+                                        ( xbmc.getLocalizedString( 30903 ), "espanol", ),
+                                        #( xbmc.getLocalizedString( 30904 ), "live", ),
+                                    )
+            elif ( self.args.category == "categories" ):
+                categories = (
+                                        ( xbmc.getLocalizedString( 30950 ), "most_popular", ),
+                                        ( xbmc.getLocalizedString( 30951 ), "by_section_us", ),
+                                        ( xbmc.getLocalizedString( 30952 ), "by_section_world", ),
+                                        ( xbmc.getLocalizedString( 30953 ), "by_section_politics", ),
+                                        ( xbmc.getLocalizedString( 30954 ), "by_section_showbiz", ),
+                                        ( xbmc.getLocalizedString( 30955 ), "by_section_crime", ),
+                                        ( xbmc.getLocalizedString( 30956 ), "by_section_funny_news", ),
+                                        ( xbmc.getLocalizedString( 30957 ), "by_section_tech", ),
+                                        ( xbmc.getLocalizedString( 30958 ), "by_section_living", ),
+                                        ( xbmc.getLocalizedString( 30959 ), "by_section_health", ),
+                                        ( xbmc.getLocalizedString( 30960 ), "by_section_student", ),
+                                        ( xbmc.getLocalizedString( 30961 ), "by_section_business", ),
+                                        ( xbmc.getLocalizedString( 30962 ), "by_section_sports", ),
+                                        ( xbmc.getLocalizedString( 30963 ), "by_section_weather", ),
+                                        ( xbmc.getLocalizedString( 30964 ), "top_stories", ),
+                                    )
+            elif ( self.args.category == "ireports" ):
+                categories = (
+                                        ( xbmc.getLocalizedString( 30965 ), "ireport_newsiest_now", ),
+                                        ( xbmc.getLocalizedString( 30966 ), "ireport_on_cnn", ),
+                                        ( xbmc.getLocalizedString( 30967 ), "ireport_sound_off", ),
+                                        ( xbmc.getLocalizedString( 30968 ), "ireport_off_beat", ),
+                                        ( xbmc.getLocalizedString( 30969 ), "cnni_programs_ireport", ),
+                                    )
+            elif ( self.args.category == "ontv" ):
+                categories = (
+                                        ( xbmc.getLocalizedString( 30980 ), "cnn_programs_american_morning", ),
+                                    )
+            elif ( self.args.category == "espanol" ):
+                categories = (
+                                        ( xbmc.getLocalizedString( 30970 ), "spanish_eleccions", ),
+                                        ( xbmc.getLocalizedString( 30971 ), "spanish_economia", ),
+                                        ( xbmc.getLocalizedString( 30972 ), "spanish_tu_dinero", ),
+                                        ( xbmc.getLocalizedString( 30973 ), "spanish_vida", ),
+                                        ( xbmc.getLocalizedString( 30974 ), "spanish_entretenimiento", ),
+                                        ( xbmc.getLocalizedString( 30975 ), "spanish_tecnologia", ),
+                                        ( xbmc.getLocalizedString( 30976 ), "spanish_estados_unidos", ),
+                                        ( xbmc.getLocalizedString( 30977 ), "spanish_america_latina", ),
+                                        ( xbmc.getLocalizedString( 30978 ), "spanish_spanish_mundo", ),
+                                    )
             # fill media list
-            ok = self._fill_media_list( parser.categories )
+            ok = self._fill_media_list( categories )
         except:
             # oops print error message
             print "ERROR: %s::%s (%d) - %s" % ( self.__class__.__name__, sys.exc_info()[ 2 ].tb_frame.f_code.co_name, sys.exc_info()[ 2 ].tb_lineno, sys.exc_info()[ 1 ], )
             ok = False
+        if ( ok ):
+            xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_LABEL )
+            # set our plugin category
+            xbmcplugin.setPluginCategory( handle=int( sys.argv[ 1 ] ), category=self.args.title )
         # send notification we're finished, successfully or unsuccessfully
         xbmcplugin.endOfDirectory( handle=int( sys.argv[ 1 ] ), succeeded=ok )
 
@@ -94,14 +104,19 @@ class Main:
         try:
             ok = True
             # enumerate through the list of categories and add the item to the media list
-            for category in categories:
-                url = "%s?title=%s&url=%s" % ( sys.argv[ 0 ], category.title, category.url, )
+            for title, category in categories:
+                if ( not sys.argv[ 2 ] ):
+                    url = "%s?title=%s&category=%s" % ( sys.argv[ 0 ], urllib.quote_plus( repr( title ) ), urllib.quote_plus( repr( category ) ), )
+                else:
+                    url = "%s?title=%s&url=%s" % ( sys.argv[ 0 ], urllib.quote_plus( repr( "%s - %s" % ( self.args.title, title, ) ) ), urllib.quote_plus( repr( self.BASE_URL % ( category, ) ) ), )
                 # check for a valid custom thumbnail for the current category
-                thumbnail = self._get_thumbnail( category.title )
+                thumbnail = self._get_thumbnail( category )
                 # set the default icon
                 icon = "defaultfolder.png"
                 # only need to add label, icon and thumbnail, setInfo() and addSortMethod() takes care of label2
-                listitem=xbmcgui.ListItem( category.title, iconImage=icon, thumbnailImage=thumbnail )
+                listitem=xbmcgui.ListItem( title, iconImage=icon, thumbnailImage=thumbnail )
+                # add the different infolabels we want to sort by
+                listitem.setInfo( type="Video", infoLabels={ "Title": title } )
                 # add the item to the media list
                 ok = xbmcplugin.addDirectoryItem( handle=int( sys.argv[ 1 ] ), url=url, listitem=listitem, isFolder=True, totalItems=len( categories ) )
                 # if user cancels, call raise to exit loop
