2e1ed59b688b1d4a85f203e391f71006078cabc8
nuka1195 <nuka1195@753fbab9-553a-0410-a42e-33068982fbc4>
2009-06-11 14:19:46 +0000

    [weather.com plus]
    -changed: version -> 1.1
    -added: compatibility check
    -changed: enable plugin settings
    
    #requires XBMC revision 21010 or newer

--- trunk/plugins/weather/weather.com plus/default.py
+++ trunk/plugins/weather/weather.com plus/default.py
@@ -11,11 +11,34 @@ __pluginname__ = "weather.com+"
 __author__ = "nuka1195"
 __url__ = "http://code.google.com/p/xbmc-addons/"
 __svn_url__ = "http://xbmc-addons.googlecode.com/svn/trunk/plugins/weather/Weather.com"
-__version__ = "1.0.1"
+__version__ = "1.1"
 
-xbmc.log( "[PLUGIN] '%s: version %s' initialized!" % ( __pluginname__, __version__, ), xbmc.LOGNOTICE )
+__XBMC_Revision__ = "21010"
+
+def _check_compatible():
+    try:
+        # spam plugin statistics to log
+        xbmc.log( "[PLUGIN] '%s: Version - %s-r%s' initialized!" % ( __plugin__, __version__, __svn_revision__.replace( "$", "" ).replace( "Revision:", "" ).strip() ), xbmc.LOGNOTICE )
+        # get xbmc revision
+        xbmc_version = xbmc.getInfoLabel( "System.BuildVersion" )
+        xbmc_rev = int( xbmc_version.split( " " )[ 1 ].replace( "r", "" ) )
+        # compatible?
+        ok = xbmc_rev >= int( __XBMC_Revision__ )
+    except:
+        # error, so unknown, allow to run
+        xbmc_rev = 0
+        ok = 2
+    # spam revision info
+    xbmc.log( "     ** Required XBMC Revision: r%s **" % ( __XBMC_Revision__, ), xbmc.LOGNOTICE )
+    xbmc.log( "     ** Found XBMC Revision: r%d [%s] **" % ( xbmc_rev, ( "Not Compatible", "Compatible", "Unknown", )[ ok ], ), xbmc.LOGNOTICE )
+    #return result
+    return ok
 
 # Start the main plugin
 if ( __name__ == "__main__" ):
-    from resources.lib.xbmcplugin_weather import Main
-    Main()
+    if ( _check_compatible() ):
+        from resources.lib.xbmcplugin_weather import Main
+        Main()
+    else:
+        raise
+
