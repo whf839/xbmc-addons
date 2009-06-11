879aeead62274c9d63c68441907b346fb49205a7
stacked.xbmc <stacked.xbmc@753fbab9-553a-0410-a42e-33068982fbc4>
2009-06-11 02:49:43 +0000

    [Justin.tv] - fixed plugin to work with Justin.tv recent changes - Added support for all the available languages on Justin.tv (see plugin settings)

--- plugins/video/Justin.tv/default.py
+++ plugins/video/Justin.tv/default.py
@@ -2,8 +2,8 @@
 __scriptname__ = "Justin.tv"
 __author__ = 'stacked [http://xbmc.org/forum/member.php?u=26908]'
 __svn_url__ = "https://xbmc-addons.googlecode.com/svn/trunk/plugins/video/Justin.tv"
-__date__ = '2009-05-26'
-__version__ = "r1021"
+__date__ = '2009-06-10'
+__version__ = "r1046"
 
 import xbmc, xbmcgui, xbmcplugin, urllib2, urllib, re, string, sys, os, traceback
 from urllib2 import Request, urlopen, URLError, HTTPError
@@ -11,23 +11,107 @@ HEADER = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.10) Gecko/200
 THUMBNAIL_PATH = os.path.join(os.getcwd().replace( ";", "" ),'resources','media')
 
 def showCategories():
-	url='http://www.justin.tv/directory?order=hot&lang=en'
+	if xbmcplugin.getSetting('language') == '0':
+		idd = 'en'
+		mode = '0'
+	elif xbmcplugin.getSetting('language') == '1':
+		idd = 'ar'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '2':
+		idd = 'bg'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '3':
+		idd = 'ca'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '4':
+		idd = 'de'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '5':
+		idd = 'el'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '6':
+		idd = 'es'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '7':
+		idd = 'fr'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '8':
+		idd = 'hi'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '9':
+		idd = 'hu'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '10':
+		idd = 'id'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '11':
+		idd = 'is'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '12':
+		idd = 'it'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '13':
+		idd = 'iw'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '14':
+		idd = 'ja'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '15':
+		idd = 'ko'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '16':
+		idd = 'lt'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '17':
+		idd = 'nl'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '18':
+		idd = 'no'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '19':
+		idd = 'pl'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '20':
+		idd = 'pt'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '21':
+		idd = 'ro'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '22':
+		idd = 'ru'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '23':
+		idd = 'sr'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '24':
+		idd = 'tl'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '25':
+		idd = 'tr'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '26':
+		idd = 'uk'
+		mode = '1'
+	elif xbmcplugin.getSetting('language') == '27':
+		idd = 'zh-TW'
+		mode = '1'
+	url='http://www.justin.tv/directory?order=hot&lang='+idd
 	req = urllib2.Request(url)
 	req.add_header('User-Agent', HEADER)
 	f=urllib2.urlopen(req)
 	a=f.read()
 	f.close()
 	match=re.compile('<div id="category_chooser">(.+?)<div class="section">', re.DOTALL).findall(a)
-	cat=re.compile('<a href="(.+?)" class="category_link">(.+?)</a> \n                    (.+?)\n').findall(match[0])
-	all=re.compile('<span class=\'not_linked\'>All</span> \n            (.+?)\n').findall(match[0])
-	name = 'All '+all[0]
+	cat=re.compile('<a href="(.+?)" class="category_link">(.+?)</a>').findall(match[0])
+	#all=re.compile('<span class=\'not_linked\'>All</span> \n            (.+?)\n').findall(match[0])
+	name = 'All '#+all[0]
 	li=xbmcgui.ListItem(name)
 	u=sys.argv[0]+"?mode=1&name="+urllib.quote_plus(name)+"&url="+urllib.quote_plus(url)
 	xbmcplugin.addDirectoryItem(int(sys.argv[1]),u,li,True)
-	for url,name,total in cat:
+	for url,name in cat:
 		url='http://www.justin.tv' + url
-		li=xbmcgui.ListItem(name+' '+total.lstrip())
-		u=sys.argv[0]+"?mode=0&name="+urllib.quote_plus(name)+"&url="+urllib.quote_plus(url)
+		li=xbmcgui.ListItem(name)
+		u=sys.argv[0]+"?mode="+mode+"&name="+urllib.quote_plus(name)+"&url="+urllib.quote_plus(url)
 		xbmcplugin.addDirectoryItem(int(sys.argv[1]),u,li,True)
 	li=xbmcgui.ListItem('(Search)')
 	u=sys.argv[0]+"?mode=3"
@@ -43,15 +127,14 @@ def showSubCategories(url, name):
 	a=f.read()
 	f.close()
 	match=re.compile('<div id="subcategory_chooser">(.+?)<div id="category_chooser">', re.DOTALL).findall(a)
-	cat=re.compile('<a href="(.+?)" class="category_link">(.+?)</a> \n                    (.+?)\n').findall(match[0])
-	all=re.compile('<span class=\'not_linked\'>All</span> \n            (.+?)\n').findall(match[0])
-	name = 'All '+all[0].lstrip()
+	cat=re.compile('<a href="(.+?)" class="category_link">(.+?)</a>').findall(match[0])
+	name = 'All '
 	li=xbmcgui.ListItem(name)
 	u=sys.argv[0]+"?mode=1&name="+urllib.quote_plus(name)+"&url="+urllib.quote_plus(url)
 	xbmcplugin.addDirectoryItem(int(sys.argv[1]),u,li,True)
-	for url,name,total in cat:
+	for url,name in cat:
 		url='http://www.justin.tv' + url
-		li=xbmcgui.ListItem(name+' '+total.lstrip())
+		li=xbmcgui.ListItem(name)
 		u=sys.argv[0]+"?mode=1&name="+urllib.quote_plus(name)+"&url="+urllib.quote_plus(url)
 		xbmcplugin.addDirectoryItem(int(sys.argv[1]),u,li,True)
 
@@ -63,11 +146,18 @@ def showLinks(url, name):
 	a=f.read()
 	f.close()
 	match=re.compile('<div class="grid_item_container"(.+?)<div id="pagelinks">', re.DOTALL).findall(a)
+	if len(match) == 0:
+		dialog = xbmcgui.Dialog()
+		ok = dialog.ok('Justin.tv', 'Error: No live streams available.')
+		showCategories()
+		return
 	cat=re.compile('<h3 class="title"(.+?)<a href="(.+?)">(.*?)</a>', re.DOTALL).findall(match[0])
 	data=re.compile('<a href="(.+?)"><img alt="" class="li_pic_125o i125x94 lateload" src1="(.+?)"').findall(a)
+	stat1=re.compile('<em>Viewers</em>:&nbsp;<span>(.+?)</span><br />').findall(match[0])
+	stat2=re.compile('<em>Views</em>:&nbsp;<span>(.+?)</span><br />').findall(match[0])
 	x=0
 	for url,thumb in data:
-		name=str(int(x+1)+(36*(page-1)))+'. '+cat[x][2]
+		name=str(int(x+1)+(36*(page-1)))+'. '+cat[x][2]+' - Viewers: '+stat1[x]+' - Views: '+stat2[x]
 		url=url.replace('/','')
 		li=xbmcgui.ListItem(name, iconImage=thumb, thumbnailImage=thumb)
 		u=sys.argv[0]+"?mode=2&name="+urllib.quote_plus(name)+"&url="+urllib.quote_plus(url)
