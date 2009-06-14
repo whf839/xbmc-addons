4e4a9c9afa1723b2a051ee400a97e96e645ec362
nuka1195 <nuka1195@753fbab9-553a-0410-a42e-33068982fbc4>
2009-06-14 18:15:33 +0000

    [flickr]
    -changed: version -> 1.5.2
    -fixed: DefaultFolder thumb did not show

--- trunk/plugins/pictures/flickr/FlickrAPI/xbmcplugin_categories.py
+++ trunk/plugins/pictures/flickr/FlickrAPI/xbmcplugin_categories.py
@@ -201,7 +201,7 @@ class Main:
                 # check for a valid custom thumbnail for the current method
                 thumbnail = thumbnail or self._get_thumbnail( method )
                 # set the default icon
-                icon = "defaultfolderBig.png"
+                icon = "DefaultFolder.png"
                 # only need to add label, icon and thumbnail, setInfo() and addSortMethod() takes care of label2
                 listitem=xbmcgui.ListItem( ltitle, iconImage=icon, thumbnailImage=thumbnail )
                 # add the item to the media list
@@ -231,5 +231,5 @@ class Main:
             thumbnail = os.path.join( self.BASE_PLUGIN_THUMBNAIL_PATH, title + ".png" )
             # use a default thumbnail if a custom thumbnail does not exists
             if ( not os.path.isfile( thumbnail ) ):
-                thumbnail = "DefaultFolderBig.png"
+                thumbnail = "DefaultFolder.png"
         return thumbnail
