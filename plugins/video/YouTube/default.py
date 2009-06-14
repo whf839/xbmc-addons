078a634b4324b5400509e66cab304544da09f7b9
nuka1195 <nuka1195@753fbab9-553a-0410-a42e-33068982fbc4>
2009-06-14 17:56:12 +0000

    [YouTube]
    -changed: version -> 1.6.8
    -changed: added some fake headers

--- plugins/video/YouTube/default.py
+++ plugins/video/YouTube/default.py
@@ -11,7 +11,7 @@ __plugin__ = "YouTube"
 __author__ = "nuka1195"
 __url__ = "http://code.google.com/p/xbmc-addons/"
 __svn_url__ = "http://xbmc-addons.googlecode.com/svn/trunk/plugins/video/YouTube"
-__version__ = "1.6.7"
+__version__ = "1.6.8"
 __XBMC_Revision__ = "19457"
 
 
