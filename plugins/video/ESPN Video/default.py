979b07e531bdedf792cfbbb513a5b4149bbdea30
stacked.xbmc <stacked.xbmc@753fbab9-553a-0410-a42e-33068982fbc4>
2009-07-14 05:18:52 +0000

    [ESPN Video] - fixed Library mode problem - updated main icon - fixed a few minor problems in the menus - made some changes to the download feature

--- plugins/video/ESPN Video/default.py
+++ plugins/video/ESPN Video/default.py
@@ -2,8 +2,8 @@
 __scriptname__ = "XBMC Video Plugin"
 __author__ = 'stacked [http://xbmc.org/forum/member.php?u=26908]'
 __svn_url__ = "https://xbmc-addons.googlecode.com/svn/trunk/plugins/video/ESPN%20Video"
-__date__ = '29-03-2009'
-__version__ = "r874"
+__date__ = '13-07-2009'
+__version__ = "1.1"
 
 import xbmc, xbmcgui, xbmcplugin, urllib2, urllib, re, string, sys, os, traceback
 THUMBNAIL_PATH = os.path.join(os.getcwd().replace( ";", "" ),'resources','media')
@@ -11,43 +11,47 @@ THUMBNAIL_PATH = os.path.join(os.getcwd().replace( ";", "" ),'resources','media'
 
 def showRoot():
 		li=xbmcgui.ListItem("Categories")
-		u=sys.argv[0]+"?mode=3"
+		u=sys.argv[0]+"?mode=3&name="+urllib.quote_plus("Categories")
 		xbmcplugin.addDirectoryItem(int(sys.argv[1]),u,li,True)
 		li=xbmcgui.ListItem("Original Digitals")
-		u=sys.argv[0]+"?mode=4"
+		u=sys.argv[0]+"?mode=4&name="+urllib.quote_plus("Original Digitals")
 		xbmcplugin.addDirectoryItem(int(sys.argv[1]),u,li,True)
 		li=xbmcgui.ListItem("TV Shows")
-		u=sys.argv[0]+"?mode=5"
+		u=sys.argv[0]+"?mode=5&name="+urllib.quote_plus("TV Shows")
 		xbmcplugin.addDirectoryItem(int(sys.argv[1]),u,li,True)
 		li=xbmcgui.ListItem("PodCenter")
-		u=sys.argv[0]+"?mode=6"
+		u=sys.argv[0]+"?mode=6&name="+urllib.quote_plus("PodCenter")
 		xbmcplugin.addDirectoryItem(int(sys.argv[1]),u,li,True)
 
 def showCategoriesC():
-		url='http://espnradio.espn.go.com/espnradio/podcast/index'
-		req = urllib2.Request(url)
-		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.6) Gecko/2009011913 Firefox/3.0.6')
-		f=urllib2.urlopen(req)
-		a=f.read()
-		f.close()
-		p=re.compile('<div id="videoPodcasts_page1" style="display: block">(.+?)<div id="featuredvideoPodcasts" class="featured-events">', re.DOTALL)
-		o=re.compile('<a href="(.+?)"><img src="http://a.espncdn.com/i/espnradio/podcast/images/button_rss.gif"')
-		r=re.compile('<img src="(.+?)" width="90"')
-		q=re.compile('<div class="playlist-item"><h4>(.+?)</h4>')
-		title=p.findall(a)
-		urls=o.findall(title[0])
-		thumbs=r.findall(title[0])
-		names=q.findall(title[0])
-		x=0
-		print thumbs
-		for url in urls:
+	url='http://espnradio.espn.go.com/espnradio/podcast/index'
+	req = urllib2.Request(url)
+	req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.6) Gecko/2009011913 Firefox/3.0.6')
+	f=urllib2.urlopen(req)
+	a=f.read()
+	f.close()
+	p=re.compile('<div id="videoPodcasts_page1" style="display: block">(.+?)<div id="featuredvideoPodcasts" class="featured-events">', re.DOTALL)
+	o=re.compile('<a href="(.+?)"><img src="http://a.espncdn.com/i/espnradio/podcast/images/button_rss.gif"')
+	r=re.compile('<img src="(.+?)" width="90"')
+	q=re.compile('<div class="playlist-item"><h4>(.+?)</h4>')
+	title=p.findall(a)
+	urls=o.findall(title[0])
+	thumbs=r.findall(title[0])
+	names=q.findall(title[0])
+	x=0
+	count=0
+	print thumbs
+	for url in urls:
+		if url != 'http://sports.espn.go.com/espnradio/podcast/index':
 			name1=names[x]
-			name = str(int(x+1))+'. '+name1
+			name = str(int(count+1))+'. '+name1
 			thumb=thumbs[x]
 			li=xbmcgui.ListItem(name, iconImage=thumb, thumbnailImage=thumb)
-			u=sys.argv[0]+"?mode=7&name="+urllib.quote_plus(name)+"&url="+urllib.quote_plus(url)
+			u=sys.argv[0]+"?mode=7&name="+urllib.quote_plus(name1)+"&url="+urllib.quote_plus(url)
 			xbmcplugin.addDirectoryItem(int(sys.argv[1]),u,li,True)
-			x=x+1
+			count=count+1
+		x=x+1
+
 
 def RSS(url):
 		req = urllib2.Request(url)
@@ -56,17 +60,19 @@ def RSS(url):
 		a=f.read()
 		f.close()
 		p=re.compile('<item>(.+?)<title>(.+?)</title>', re.DOTALL)
-		o=re.compile('<enclosure url="(.+?)"')
+		thumbs=re.compile('<url>(.+?)jpg</url>').findall(a)
+		o=re.compile('<enclosure url="http://(.+?)\?prm=(.+?)x(.+?)"')
 		match=p.findall(a)
 		URLS=o.findall(a)
 		x=0
+		thumb=thumbs[0]+'jpg'
 		for title in match:
-			name1 = match[x][1]
+			name1 = clean(match[x][1])
 			name = str(int(x+1))+'. '+name1
-			url=URLS[x]
-			li=xbmcgui.ListItem(name)
+			url=URLS[x][2]
+			li=xbmcgui.ListItem(name, iconImage=thumb, thumbnailImage=thumb)
 			li.setInfo( type="Video", infoLabels={ "Title": name } )
-			u=sys.argv[0]+"?mode=2&name="+urllib.quote_plus(name)+"&url="+urllib.quote_plus(url)
+			u=sys.argv[0]+"?mode=2&name="+urllib.quote_plus(name1)+"&url="+urllib.quote_plus(url)
 			xbmcplugin.addDirectoryItem(int(sys.argv[1]),u,li)
 			x=x+1
 		
@@ -120,7 +126,8 @@ def showCategoriesA():
 			num=num.replace(')','')
 			url='http://espn.go.com/video/beta/libraryPlaylist?categoryid='+num
 			test=info[x][2]
-			name=test[:-4]
+			test=test.replace('<img src="http://assets.espn.go.com/i/in.gif"> ','')
+			name=test[:-3]
 			name=name.replace('(','')
 			if (name=='The Latest'):
 				name = '> The Latest'
@@ -151,7 +158,7 @@ def showCategories():
 			test=info[x][2]
 			name=test[:-4]
 			name=name.replace('(','')
-			if (name=='The Latest '):
+			if (name=='The Latest'):
 				name = '> The Latest'
 			name=name.replace('<BR>',' ')
 			li=xbmcgui.ListItem(name)
@@ -160,6 +167,8 @@ def showCategories():
 			x=x+1
 
 def showListB(url,name):
+		print url
+		cat_title=name
 		thisurl=url
 		req = urllib2.Request(url+'&pageNum='+str(int(page))+'&assetURL=http://assets.espn.go.com')
 		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.6) Gecko/2009011913 Firefox/3.0.6')
@@ -182,10 +191,11 @@ def showListB(url,name):
 			xbmcplugin.addDirectoryItem(int(sys.argv[1]),u,li)
 			x=x+1
 		li=xbmcgui.ListItem(xbmc.getLocalizedString( 30004 ),iconImage="DefaultVideo.png", thumbnailImage=os.path.join(THUMBNAIL_PATH, 'next.png'))
-		u=sys.argv[0]+"?mode=8&name="+urllib.quote_plus(name)+"&url="+urllib.quote_plus(thisurl)+"&page="+str(int(page)+1)
+		u=sys.argv[0]+"?mode=8&name="+urllib.quote_plus(cat_title)+"&url="+urllib.quote_plus(thisurl)+"&page="+str(int(page)+1)
 		xbmcplugin.addDirectoryItem(int(sys.argv[1]),u,li,True)
 
 def showList(url,name):
+		print url
 		req = urllib2.Request(url)
 		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.6) Gecko/2009011913 Firefox/3.0.6')
 		f=urllib2.urlopen(req)
@@ -215,19 +225,20 @@ def Update(__version__):
 			newVersion=link
 			dia = xbmcgui.Dialog()
 			ok = dia.ok(xbmc.getLocalizedString( 30019 ), xbmc.getLocalizedString( 30020 )+'\n\n'+xbmc.getLocalizedString( 30021 )+' '+__version__+'\n'+xbmc.getLocalizedString( 30022 )+' '+newVersion)
-			
-def playVideo(url, name):
-	vid = url
-	vid2 = vid
-	vid = vid.rsplit('/')
-	ck = vid2.rsplit('.')
-	if (ck[0] == 'http://podloc'):
-		name = vid[12]
-	else:
-		name = vid[4]
+
+def clean(name):
+    remove=[('&amp;','&'),('&quot;','\"')]
+    for old, new in remove:
+        name=name.replace(old,new)
+    return name
+	
+def playVideo(url,name):
+	title=name
+	name=clean_file(name)
+	name=name[:+32]
 	def Download(url,dest):
 			dp = xbmcgui.DialogProgress()
-			dp.create('Downloading','',name)
+			dp.create('Downloading',title,'Filename: '+name+ '.flv')
 			urllib.urlretrieve(url,dest,lambda nb, bs, fs, url=url: _pbhook(nb,bs,fs,url,dp))
 	def _pbhook(numblocks, blocksize, filesize, url=None,dp=None):
 			try:
@@ -240,18 +251,30 @@ def playVideo(url, name):
 					dp.close()
 	flv_file = None
 	stream = 'false'
-	drive = xbmc.translatePath( ( "U:\\" , "Q:\\" )[ os.environ.get( "OS", "xbox" ) == "xbox" ] )
-	tempDir = os.path.join( drive, 'cache' )
-	if not os.path.isdir( tempDir) :
-		os.makedirs( tempDir )
 	if (xbmcplugin.getSetting('download') == 'true'):
-			flv_file = xbmc.translatePath(os.path.join(xbmcplugin.getSetting('download_Path'), name))
-			Download(url,flv_file)
+		if (xbmcplugin.getSetting('ask_filename') == 'true'):
+			searchStr = name
+			keyboard = xbmc.Keyboard(searchStr, "Save as:")
+			keyboard.doModal()
+			if (keyboard.isConfirmed() == False):
+				return
+			searchstring = keyboard.getText()
+			name=searchstring
+		flv_file = xbmc.translatePath(os.path.join(xbmcplugin.getSetting('download_Path'), name + '.flv' ))
+		Download(url,flv_file)
 	elif (xbmcplugin.getSetting('download') == 'false' and xbmcplugin.getSetting('download_ask') == 'true'):
 		dia = xbmcgui.Dialog()
-		ret = dia.select(xbmc.getLocalizedString( 30005 ), [xbmc.getLocalizedString( 30001 ), xbmc.getLocalizedString( 30007 ), xbmc.getLocalizedString( 30006 )])
+		ret = dia.select('What do you want to do?', ['Download & Play', 'Stream', 'Exit'])
 		if (ret == 0):
-			flv_file = xbmc.translatePath(os.path.join(xbmcplugin.getSetting('download_Path'), name))
+			if (xbmcplugin.getSetting('ask_filename') == 'true'):
+				searchStr = name
+				keyboard = xbmc.Keyboard(searchStr, "Save as:")
+				keyboard.doModal()
+				if (keyboard.isConfirmed() == False):
+					return
+				searchstring = keyboard.getText()
+				name=searchstring
+			flv_file = xbmc.translatePath(os.path.join(xbmcplugin.getSetting('download_Path'), name + '.flv'))
 			Download(url,flv_file)
 		elif (ret == 1):
 			stream = 'true'
@@ -263,11 +286,19 @@ def playVideo(url, name):
 		player_type = xbmc.PLAYER_CORE_DVDPLAYER
 	else:
 		player_type = xbmc.PLAYER_CORE_MPLAYER
+	g_thumbnail = xbmc.getInfoImage( "ListItem.Thumb" )
+	listitem=xbmcgui.ListItem(title ,iconImage="DefaultVideo.png", thumbnailImage=g_thumbnail)
 	if (flv_file != None and os.path.isfile(flv_file)):
-		xbmc.Player(player_type).play(str(flv_file))
+		xbmc.Player(player_type).play(str(flv_file), listitem)
 	elif (stream == 'true'):
-		xbmc.Player(player_type).play(str(url))
+		xbmc.Player(player_type).play(str(url), listitem)
 	xbmc.sleep(200)
+	
+def clean_file(name):
+    remove=[(':',' - '),('\"',''),('|',''),('>',''),('<',''),('?',''),('*','')]
+    for old, new in remove:
+        name=name.replace(old,new)
+    return name
 
 def get_params():
         param=[]
@@ -310,6 +341,7 @@ except:
         pass
 
 if mode==None:
+	name=''
 	if xbmcplugin.getSetting("update") == "true":
 		Update(__version__)
 	showRoot()
@@ -330,6 +362,8 @@ elif mode==7:
 elif mode==8:
 	showListB(url, name)
 
+xbmcplugin.setPluginCategory(int(sys.argv[1]), name )
+xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_LABEL )
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
     
