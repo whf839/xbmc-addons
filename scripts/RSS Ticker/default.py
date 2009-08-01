3d2fdbad080aad5ccba5e20675ed1b1b7d9c1b49
rwparris2 <rwparris2@753fbab9-553a-0410-a42e-33068982fbc4>
2009-08-01 12:48:28 +0000

    [RSS Ticker] initial commit

---
+++ scripts/RSS Ticker/default.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import xbmc
+
+# Script constants
+__scriptname__ = "RSS Ticker Manager"
+__author__ = "rwparris2"
+__url__ = "http://code.google.com/p/xbmc-addons/"
+__credits__ = "Team XBMC"
+__version__ = "1.0"
+
+xbmc.log( "[SCRIPT] '%s: version %s' initialized!" % ( __scriptname__, __version__, ), xbmc.LOGNOTICE )
+
+if ( __name__ == "__main__" ):
+    import resources.lib.gui as gui
+    ui = gui.GUI( "rssTicker.xml", os.getcwd(), "Default" )
+    ui.doModal()
+    del ui
+    sys.modules.clear()
