5d13a91bbe173f0660931d44f5d9b69751d85b8f
nuka1195 <nuka1195@753fbab9-553a-0410-a42e-33068982fbc4>
2009-08-02 15:58:21 +0000

    [weather.com plus]
    -changed: version -> 1.1.7c
    -changed: compatible check

--- trunk/plugins/weather/weather.com plus/default.py
+++ trunk/plugins/weather/weather.com plus/default.py
@@ -12,14 +12,14 @@ __pluginname__ = "weather.com+"
 __author__ = "nuka1195"
 __url__ = "http://code.google.com/p/xbmc-addons/"
 __svn_url__ = "http://xbmc-addons.googlecode.com/svn/trunk/plugins/weather/Weather.com"
-__version__ = "1.1.7b"
+__version__ = "1.1.7c"
 __svn_revision__ = "$Revision$"
 __XBMC_Revision__ = "21010"
 
 def _check_compatible():
     try:
         # spam plugin statistics to log
-        xbmc.log( "[PLUGIN] '%s: Version - %s-r%s' initialized!" % ( __plugin__, __version__, __svn_revision__.replace( "$", "" ).replace( "Revision:", "" ).strip() ), xbmc.LOGNOTICE )
+        xbmc.log( "[PLUGIN] '%s: Version - %s-r%s' initialized!" % ( __plugin__, __version__, __svn_revision__.replace( "$", "" ).replace( "Revision", "" ).replace( ":", "" ).strip() ), xbmc.LOGNOTICE )
         # get xbmc revision
         xbmc_rev = int( xbmc.getInfoLabel( "System.BuildVersion" ).split( " r" )[ -1 ] )
         # compatible?
