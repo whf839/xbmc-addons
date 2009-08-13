67c4fda8d9764ed91415ad22ff23c78feeb6eeed
pajret <pajret@753fbab9-553a-0410-a42e-33068982fbc4>
2009-08-13 16:11:39 +0000

    [widelec.org] - fixed - problem with listing one-page galleries..

--- trunk/plugins/pictures/widelec.org/default.py
+++ trunk/plugins/pictures/widelec.org/default.py
@@ -5,11 +5,12 @@ import urllib,urllib2,re,xbmcplugin,xbmcgui,os
 #.nF0
 __plugin__  = "widelec.org"
 __author__  = "pajretX"
-__date__    = "12 August 2009"
-__version__ = "1.36"
+__date__    = "13 August 2009"
+__version__ = "1.37"
 
 HOME_DIR = os.getcwd()
 base = "http://www.widelec.org/"
+
 # STRINGS
 names = xbmc.Language( HOME_DIR ).getLocalizedString
 naj = (names (30000))
@@ -80,43 +81,53 @@ def FOTKI(url):
 	response.close()
         foto_count=re.compile('class="next">Nast\xc4\x99pna</a><a HREF="(.+?)">Koniec</a>').findall(llink)
 	del llink
-	if len(foto_count)>1:        
-		dzzz=str(foto_count[0])
-		foto_c_no=re.compile('.+?page2=(\d+).+?').findall(dzzz)[0]
-		page_id_no = re.compile('(\d+).html').findall(url)[0]
-		for photos in range(1,int(foto_c_no) + 1):
-			srerl = ('http://www.widelec.org/index.php?page2=%s' %photos)
-			req = urllib2.Request(srerl +'site=blog&action=detail&blog_id=' + page_id_no +'&site=blog&section=1&text=&pages=10')
-			req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-			response=urllib2.urlopen(req)
-			html = response.read()
-			response.close()
-			matches=re.compile('<img src="stuff(.+?)">').findall(html)
-			del html
-			for foto in matches:
-				addLink("fotka",base +'stuff/' +foto,'')
-        elif len(foto_count)<1:
-		req = urllib2.Request(url)
-		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-		response=urllib2.urlopen(req)
-		link = response.read()
-		response.close()
-		foto_count=re.compile('<a HREF="(.+?)" class="next">Nast\xc4\x99pna</a>').findall(link)[0]
-		dzzz2=re.compile('>(\d)<').findall(foto_count)[-1]
-		for photos2 in range(1,int(dzzz2) + 1):
-			page_id_no = re.compile('(\d+).html').findall(url)[0]
-			srerl = ('http://www.widelec.org/index.php?page2=%s' %photos2)
-			req = urllib2.Request(srerl +'site=blog&action=detail&blog_id=' + page_id_no +'&site=blog&section=1&text=&pages=10')
-			req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-			response=urllib2.urlopen(req)
-			html = response.read()
-			response.close()
-			matches=re.compile('<img src="stuff(.+?)">').findall(html)
-			del html
-			for foto in matches:
-				addLink("fotka",base +'stuff/' +foto,'')
+        try:
+                if len(foto_count)>1:        
+                        dzzz=str(foto_count[0])
+                        foto_c_no=re.compile('.+?page2=(\d+).+?').findall(dzzz)[0]
+                        page_id_no = re.compile('(\d+).html').findall(url)[0]
+                        for photos in range(1,int(foto_c_no) + 1):
+                                srerl = ('http://www.widelec.org/index.php?page2=%s' %photos)
+                                req = urllib2.Request(srerl +'site=blog&action=detail&blog_id=' + page_id_no +'&site=blog&section=1&text=&pages=10')
+                                req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+                                response=urllib2.urlopen(req)
+                                html = response.read()
+                                response.close()
+                                matches=re.compile('<img src="stuff(.+?)">').findall(html)
+                                del html
+                                for foto in matches:
+                                        addLink("fotka",base +'stuff/' +foto,'')
+                elif len(foto_count)<1:
+                        req = urllib2.Request(url)
+                        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+                        response=urllib2.urlopen(req)
+                        link = response.read()
+                        response.close()
+                        foto_count=re.compile('<a HREF="(.+?)" class="next">Nast\xc4\x99pna</a>').findall(link)[0]
+                        dzzz2=re.compile('>(\d)<').findall(foto_count)[-1]
+                        for photos2 in range(1,int(dzzz2) + 1):
+                                page_id_no = re.compile('(\d+).html').findall(url)[0]
+                                srerl = ('http://www.widelec.org/index.php?page2=%s' %photos2)
+                                req = urllib2.Request(srerl +'site=blog&action=detail&blog_id=' + page_id_no +'&site=blog&section=1&text=&pages=10')
+                                req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+                                response=urllib2.urlopen(req)
+                                html = response.read()
+                                response.close()
+                                matches=re.compile('<img src="stuff(.+?)">').findall(html)
+                                del html
+                                for foto in matches:
+                                        addLink("fotka",base +'stuff/' +foto,'')
 
-	
+	except:
+                req = urllib2.Request(url)
+                req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+                response=urllib2.urlopen(req)
+                link = response.read()
+                response.close()
+                matches=re.compile('<img src="stuff(.+?)">').findall(link)
+                del link
+                for foto in matches:
+                        addLink("fotka",base +'stuff/' +foto,'')
 			
 def get_params():
 	param=[]
