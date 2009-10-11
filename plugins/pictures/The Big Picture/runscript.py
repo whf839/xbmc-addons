7e8ce865006b20f64b64df11bb82ee4ac78e9f21
rwparris2 <rwparris2@753fbab9-553a-0410-a42e-33068982fbc4>
2009-10-11 20:32:54 +0000

    [The Big Picture] - better handling of script constants cosmetics: more consistent logging

--- plugins/pictures/The Big Picture/runscript.py
+++ plugins/pictures/The Big Picture/runscript.py
@@ -1,19 +1,19 @@
 import os
 import sys
+import default
 
 # Script constants
-__scriptname__ = 'The Big Picture'
-__author__ = 'rwparris2'
-__url__ = 'http://code.google.com/p/xbmc-addons/'
-__credits__ = 'Team XBMC'
-__version__ = '1.1.1'
+__scriptname__ = sys.modules['default'].__scriptname__
+__version__ = sys.modules['default'].__version__
 
 print '[SCRIPT][%s] version %s initialized!' % (__scriptname__, __version__)
 
 if (__name__ == '__main__'):
     if sys.argv[1] == '-runscript':
         import resources.lib.gui as gui
+
         ui = gui.GUI( 'main.xml', os.getcwd(), 'default' )
         ui.doModal()
         del ui
+
         sys.modules.clear()
