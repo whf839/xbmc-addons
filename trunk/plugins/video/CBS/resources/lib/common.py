997aa1f3497a1926883e185e6101444ca5d400a4
BlueCopter <BlueCopter@753fbab9-553a-0410-a42e-33068982fbc4>
2009-10-29 17:33:06 +0000

    [CBS] added- Proxy handling for international use by lkraav  http://xbmc.org/forum/showpost.php?p=418467&postcount=110 changed- version 1.1

--- trunk/plugins/video/CBS/resources/lib/common.py
+++ trunk/plugins/video/CBS/resources/lib/common.py
@@ -71,6 +71,14 @@ def addDirectory(name, url='', mode='default', thumb='', icon='', plot=''):
 
 def getHTML( url ):
     try:
+        us_proxy = 'http://' + xbmcplugin.getSetting('us_proxy') + ':' + xbmcplugin.getSetting('us_proxy_port')
+
+        if (xbmcplugin.getSetting('us_proxy_enable') == 'true'):
+                 print 'Using proxy: ' + us_proxy
+                 proxy_handler = urllib2.ProxyHandler({'http':us_proxy})
+                 opener = urllib2.build_opener(proxy_handler)
+                 urllib2.install_opener(opener)
+
         print 'CBS --> common :: getHTML :: url = '+url
         req = urllib2.Request(url)
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
