d634718d0dc674ceb2b8196976f9bc19240b1a94
amet1977 <amet1977@753fbab9-553a-0410-a42e-33068982fbc4>
2009-11-16 11:15:08 +0000

    [OpenSubtitles_OSD] - v1.41
    - fixed Download errors on WIN

--- trunk/scripts/OpenSubtitles_OSD/default.py
+++ trunk/scripts/OpenSubtitles_OSD/default.py
@@ -8,7 +8,7 @@ __author__ = "Amet"
 __url__ = "http://code.google.com/p/opensubtitles-osd/"
 __svn_url__ = "http://xbmc-addons.googlecode.com/svn/trunk/scripts/OpenSubtitles_OSD"
 __credits__ = ""
-__version__ = "1.40"
+__version__ = "1.41"
 __XBMC_Revision__ = "22240"
 
 BASE_RESOURCE_PATH = xbmc.translatePath( os.path.join( os.getcwd(), 'resources', 'lib' ) )
