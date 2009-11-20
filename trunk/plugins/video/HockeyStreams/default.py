61ec632e6a3863a13a90f1af2c56958c3ecf947d
f3ar007 <f3ar007@753fbab9-553a-0410-a42e-33068982fbc4>
2009-11-20 03:23:58 +0000

    [hockeystreams] -update: version 1.3.0 default.py refault.py rewrite, code overhaul.

--- trunk/plugins/video/HockeyStreams/default.py
+++ trunk/plugins/video/HockeyStreams/default.py
@@ -1,289 +1,122 @@
-import urllib2, urllib, time, os, re
-import xbmcplugin, xbmcgui, xbmc
 from BeautifulSoup import BeautifulSoup
 from mechanize import Browser
+import xbmcplugin, xbmcgui, xbmc, time, os, re, urllib
 
-# hockeystreams xbmc video plugin v1.2.0b
-# stream live and archived games directly to your television!
-# http://code.google.com/p/hockeystreams
-# http://f3ar.bravehost.com/xbmc/hockeystreams/
-# f3ar007 -at- gmail.com
+username        = xbmcplugin.getSetting('username')
+password        = xbmcplugin.getSetting('password')
+hockeystreams   = 'http://hockeystreams.com'
+archivestreams  = 'http://hockeystreams.com/hockey_archives'
+empty           = None
+directLinks     = {}
+games           = {}
+hqStreams       = re.compile('/live_streams/.*[0-9]$')
+hdStreams       = re.compile('/live_streams/.*hi-qual$')
+hdpStreams      = re.compile('/live_streams/.*hi-qual-plus$')
+hqArchives      = re.compile('/hockey_archives/0/.*[0-9]$')
+hdArchives      = re.compile('/hockey_archives/0/.*hi-qual$')
+hdpArchives     = re.compile('/hockey_archives/0/.*hi-qual-plus$')
 
-def ipException(url,username,password):
-    path = os.getcwd()
-    path = os.path.join(path, 'resources')
-    date = open(path + '\\date.txt','r')
-    date = date.read()
-    if date == time.strftime('%A %B %d, %Y'):
-        print 'Exception Already Added!'
-    elif date != time.strftime('%A %B %d, %Y'):
-        br = Browser()
-        br.open(url)
-        br.select_form(nr=0)
-        br['username'] = username
-        br['password'] = password
-        br.submit()
-        br.open(url+'/include/exception.inc.php')
-        br.select_form(nr=0)
-        br.submit()
-        print 'Exception Added!'
-        date = open(path + '\\date.txt','w')
-        today = time.strftime('%A %B %d, %Y')
-        date.write(today)
-        date.close()
+def createDate():                                               ##core module
+    archiveMonth    = xbmcplugin.getSetting('archiveMonth')
+    archiveDay      = xbmcplugin.getSetting('archiveDay')
+    archiveYear     = xbmcplugin.getSetting('archiveYear')
+
+    archiveMonth    = str(int(archiveMonth)+1)
+    archiveDay      = str(int(archiveDay)+1)
+
+    if len(archiveMonth) == 1:
+        archiveMonth = '0' + str(archiveMonth)
+    elif len(archiveMonth) == 2:
+        archiveMonth = archiveMonth
+
+    if len(archiveDay) == 1:
+        archiveDay = '0' + str(archiveDay)
+    elif len(archiveMonth) == 2:
+        archiveDay = str(archiveDay)
+
+    if archiveYear == int('0') or '0':
+        archiveYear = '2009'
+    elif archiveYear == int('1') or '1':
+        archiveYear = '2010'
+
+    archiveDate = '/' + archiveMonth + '-' + archiveDay + '-' + archiveYear + '/'
+    return archiveDate
 
 def categories():
-    addDir('Live Streams','http://hockeystreams.com',1,'')
-    addDir('Hockey Archives','http://hockeystreams.com',8,'')
+    addDir('Live Streams',hockeystreams,1,'')
+    addDir('Archived Streams',hockeystreams,2,'')
+    addDir('RSS Streams',hockeystreams,3,'')
 
-def liveIndex(url):
-    addDir('HQ Games',url,2,'')
-    addDir('HD Games',url,3,'')
-    addDir('HD-Plus Games',url,4,'')
+def indexes(url,startNum):
+    addDir('High Quality Games',url,startNum,'')
+    addDir('High Definition Games',url,startNum+1,'')
+    addDir('High Definition Plus Games',url,startNum+2,'')
 
-def liveHqGames(url,usr,pwd):
-    # find game page
+def login(usr,pwd,url,selector):
     br = Browser()
-    br.open(url)
-    br.select_form(nr=0)
-    br['username'] = usr
-    br['password'] = pwd
-    resp = br.submit()
-    html = resp.read()
-    soup = BeautifulSoup(''.join(html))
-    find = soup.findAll(attrs={ 'href' : re.compile('/live_streams/.*[0-9]$') } )
-    i = 0
-    for test in find:
-        ending = str(test['href'])
-        gamePage = url + ending
-        gameName = os.path.dirname(gamePage)
-        gameName = re.sub('_|/',' ',gameName)
-        gameName = gameName[38:]
-        addDir(gameName,gamePage,5,'')
-        i = i + 1
-    
-def liveHqLinks(url,usr,pwd):
-    # find direct link on game page
-    br = Browser()
-    br.open(url)
+    br.open(hockeystreams)                                      ##default login page, not passed url
     br.select_form(nr=0)
     br['username'] = usr
     br['password'] = pwd
     br.submit()
-    resp = br.open(url)
-    html = resp.read()
-    soup = BeautifulSoup(''.join(html))
-    find = soup.findAll('input')
-    for test in find:
-        if 'direct_link' in test.get('id',''):
-            direct = test['value']
-            addLink(name,direct,'')
+    if selector == 'login':                                     ##remove useless if and replace with first elif
+        pass
+    elif selector == 'browser':
+        br.open(url)                                            ##open passed url
+        return br
+    elif selector == 'function':
+        data = br.open(url)
+        return (data,br)
 
-def liveHdGames(url,usr,pwd):
-    # find game page
-    br = Browser()
-    br.open(url)
-    br.select_form(nr=0)
-    br['username'] = usr
-    br['password'] = pwd
-    resp = br.submit()
-    html = resp.read()
-    soup = BeautifulSoup(''.join(html))
-    find = soup.findAll(attrs={ 'href' : re.compile('/live_streams/.*hi-qual$') } )
-    i = 0
-    for test in find:
-        ending = str(test['href'])
-        gamePage = url + ending
-        gameName = os.path.dirname(gamePage)
-        gameName = os.path.dirname(gameName)
-        gameName = re.sub('_|/',' ',gameName)
-        gameName = gameName[38:]
-        addDir(gameName,gamePage,6,'')
-        i = i + 1
+def getGameNameGamePage(gameType):
+    if gameType in (hqStreams,hdStreams,hdpStreams):
+        foundGames = soupIt(username,password,hockeystreams,'attrs',gameType)
+    elif gameType in (hqArchives,hdArchives,hdpArchives):
+        archiveDate = createDate()
+        foundGames = soupIt(username,password,archivestreams+archiveDate,'attrs',gameType)
+    for test in foundGames:
+        ending      = str(test['href'])
+        gamePage    = hockeystreams + ending
+        gameName    = os.path.dirname(gamePage)
+        gameName    = re.sub('_|/',' ',gameName)
+        if gameType in (hqStreams,hdStreams,hdpStreams):
+            gameName = gameName[38:]
+        elif gameType in (hqArchives,hdArchives,hdpArchives):
+            gameName = gameName[43:]
+        games[gameName] = gamePage
+    del foundGames
+    return games                                                ##return a dict with game name and page containing direct link
 
-def liveHdLinks(url,usr,pwd):
-    # find direct link on game page
-    br = Browser()
-    br.open(url)
-    br.select_form(nr=0)
-    br['username'] = usr
-    br['password'] = pwd
-    br.submit()
-    resp = br.open(url)
-    html = resp.read()
-    soup = BeautifulSoup(''.join(html))
-    find = soup.findAll('input')
-    for test in find:
-        if 'direct_link' in test.get('id',''):
-            direct = test['value']
-            addLink(name,direct,'')
+def getGameNameDirectLink(selector):
+    games = getGameNameGamePage(selector)                       ##hqStreams,hdStreams,hdpStreams,etc
+    for k,v in games.iteritems():
+        foundGames = soupIt(username,password,v,'input',empty)  ##foundGames is BeautifulSoup.resultSet
+        for test in foundGames:                                 ##get rid of this 'busy loop' in the next minor revision
+            if 'direct_link' in test.get('id',''):
+                directLink = test['value']
+                directLinks[k] = directLink
+    del selector
+    return directLinks
 
-def liveHdpGames(url,usr,pwd):
-    # find game page
-    br = Browser()
-    br.open(url)
-    br.select_form(nr=0)
-    br['username'] = usr
-    br['password'] = pwd
-    resp = br.submit()
-    html = resp.read()
-    soup = BeautifulSoup(''.join(html))
-    find = soup.findAll(attrs={ 'href' : re.compile('/live_streams/.*hi-qual-plus$') } )
-    i = 0
-    for test in find:
-        ending = str(test['href'])
-        gamePage = url + ending
-        gameName = os.path.dirname(gamePage)
-        gameName = os.path.dirname(gameName)
-        gameName = re.sub('_|/',' ',gameName)
-        gameName = gameName[38:]
-        addDir(gameName,gamePage,7,'')
-        i = i + 1
+def soupIt(usr,pwd,currentUrl,selector,gameType):
+    (data,br)   = login(username,password,currentUrl,'function')
+    html        = data.read()
+    soup        = BeautifulSoup(''.join(html))
+    if selector == 'attrs':
+        found = soup.findAll(attrs={'href':gameType})
+    elif selector == 'input':
+        found = soup.findAll('input')
+    del selector
+    return found
 
-def liveHdpLinks(url,usr,pwd):
-    # find direct link on game page
-    br = Browser()
-    br.open(url)
-    br.select_form(nr=0)
-    br['username'] = usr
-    br['password'] = pwd
-    br.submit()
-    resp = br.open(url)
-    html = resp.read()
-    soup = BeautifulSoup(''.join(html))
-    find = soup.findAll('input')
-    for test in find:
-        if 'direct_link' in test.get('id',''):
-            direct = test['value']
-            addLink(name,direct,'')
+def populateGames(selector):
+    populated = getGameNameDirectLink(selector)
+    for k,v in populated.iteritems():
+        addLink(k,v,'')
 
-def archiveIndex(url):
-    addDir('HQ Games','http://hockeystreams.com',9,'')
-    addDir('HD Games','http://hockeystreams.com',10,'')
-    addDir('HD-Plus Games','http://hockeystreams.com',11,'')
+##voinages' stuff I still don't understand, entirely.
+##---------------------------------------------------
 
-def archiveHqGames(url,usr,pwd,archiveDate):
-    # find game page
-    br = Browser()
-    br.open(url)
-    br.select_form(nr=0)
-    br['username'] = usr
-    br['password'] = pwd
-    br.submit()
-    url0 = url + '/hockey_archives/' + archiveDate
-    resp = br.open(url0)
-    html = resp.read()
-    soup = BeautifulSoup(''.join(html))
-    find = soup.findAll(attrs={ 'href' : re.compile('/hockey_archives/0/.*[0-9]$') } )
-    i = 0
-    for test in find:
-        ending = str(test['href'])
-        gamePage = url + ending
-        gameName = os.path.dirname(gamePage)
-        gameName = re.sub('_|/',' ',gameName)
-        gameName = gameName[43:]
-        addDir(gameName,gamePage,12,'')
-        i = i + 1
-
-def archiveHqLinks(url,usr,pwd):
-    # find direct link on game page
-    br = Browser()
-    br.open(url)
-    br.select_form(nr=0)
-    br['username'] = usr
-    br['password'] = pwd
-    br.submit()
-    resp = br.open(url)
-    html = resp.read()
-    soup = BeautifulSoup(''.join(html))
-    find = soup.findAll('input')
-    for test in find:
-        if 'text' in test.get('type',''):
-            direct = str(test['value'])
-            addLink(name,direct,'')
-
-def archiveHdGames(url,usr,pwd,archiveDate):
-    # find game page
-    br = Browser()
-    br.open(url)
-    br.select_form(nr=0)
-    br['username'] = usr
-    br['password'] = pwd
-    br.submit()
-    url0 = url + '/hockey_archives/' + archiveDate
-    resp = br.open(url0)
-    html = resp.read()
-    soup = BeautifulSoup(''.join(html))
-    find = soup.findAll(attrs={ 'href' : re.compile('/hockey_archives/0/.*hi-qual$') } )
-    i = 0
-    for test in find:
-        ending = str(test['href'])
-        gamePage = url + ending
-        gameName = os.path.dirname(gamePage)
-        gameName = os.path.dirname(gameName)
-        gameName = re.sub('_|/',' ',gameName)
-        gameName = gameName[43:]
-        addDir(gameName,gamePage,13,'')
-        i = i + 1
-
-def archiveHdLinks(url,usr,pwd):
-    # find direct link on game page
-    br = Browser()
-    br.open(url)
-    br.select_form(nr=0)
-    br['username'] = usr
-    br['password'] = pwd
-    br.submit()
-    resp = br.open(url)
-    html = resp.read()
-    soup = BeautifulSoup(''.join(html))
-    find = soup.findAll('input')
-    for test in find:
-        if 'text' in test.get('type',''):
-            direct = str(test['value'])
-            addLink(name,direct,'')
-            
-def archiveHdpGames(url,usr,pwd,archiveDate):
-    # find game page
-    br = Browser()
-    br.open(url)
-    br.select_form(nr=0)
-    br['username'] = usr
-    br['password'] = pwd
-    br.submit()
-    url0 = url + '/hockey_archives/' + archiveDate
-    resp = br.open(url0)
-    html = resp.read()
-    soup = BeautifulSoup(''.join(html))
-    find = soup.findAll(attrs={ 'href' : re.compile('/hockey_archives/0/.*hi-qual-plus$') } )
-    i = 0
-    for test in find:
-        ending = str(test['href'])
-        gamePage = url + ending
-        gameName = os.path.dirname(gamePage)
-        gameName = os.path.dirname(gameName)
-        gameName = re.sub('_|/',' ',gameName)
-        gameName = gameName[43:]
-        addDir(gameName,gamePage,14,'')
-        i = i + 1
-    
-def archiveHdpLinks(url,usr,pwd):
-    # find direct link on game page
-    br = Browser()
-    br.open(url)
-    br.select_form(nr=0)
-    br['username'] = usr
-    br['password'] = pwd
-    br.submit()
-    resp = br.open(url)
-    html = resp.read()
-    soup = BeautifulSoup(''.join(html))
-    find = soup.findAll('input')
-    for test in find:
-        if 'text' in test.get('type',''):
-            direct = str(test['value'])
-            addLink(name,direct,'')
-            
 def get_params():
     param=[]
     paramstring=sys.argv[2]
@@ -299,7 +132,6 @@ def get_params():
                     splitparams=pairsofparams[i].split('=')
                     if (len(splitparams))==2:
                             param[splitparams[0]]=splitparams[1]
-
     return param
 
 def addDir(name,url,mode,iconimage):
@@ -315,83 +147,60 @@ def addLink(name,url,iconimage):
     point.setInfo( type='Video', infoLabels={ 'Title': name } )
     ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=point)
 
-params = get_params()
-url = None
-name = None
-mode = None
-username = xbmcplugin.getSetting('username')
-password = xbmcplugin.getSetting('password')
-archiveMonth = xbmcplugin.getSetting('archiveMonth')
-archiveDay = xbmcplugin.getSetting('archiveDay')
-archiveYear = xbmcplugin.getSetting('archiveYear')
-archiveMonth = str(int(archiveMonth) + 1)
-archiveDay = str(int(archiveDay) + 1)
+##---------------------------------------------------
+##end of voinages' stuff.
 
-if len(archiveMonth) == 1:
-    archiveMonth = '0' + str(archiveMonth)
-elif len(archiveMonth) == 2:
-    archiveMonth = str(archiveMonth)
-    
-if len(archiveDay) == 1:
-    archiveDay = '0' + str(archiveDay)
-elif len(archiveDay) == 2:
-    archiveDay = str(archiveDay)
-    
-if archiveYear == int('0') or '0':
-    archiveYear = '2009'
-elif archiveYear == int('1') or '1':
-    archiveYear = '2010'
-    
-archiveDate = '/' + str(archiveMonth) + '-' + str(archiveDay) + '-' + str(archiveYear) + '/'
-str(archiveDate)
-
-hs = 'http://hockeystreams.com'
+params  = get_params()
+url     = None
+name    = None
+mode    = None
 
 try:
-    url = urllib.unquote_plus(params['url'])
+    url     = urllib.unquote_plus(params['url'])
 except:
     pass
 try:
-    name = urllib.unquote_plus(params['name'])
+    name    = urllib.unquote_plus(params['name'])
 except:
     pass
 try:
-    mode=int(params['mode'])
+    mode    = int(params['mode'])
 except:
     pass
-
-print 'archiveDate: '+str(archiveDate)
 
 if mode == None or url == None or len(url)<1:
-    ipException(hs,username,password)
     categories()
 elif mode == 1:
-    liveIndex(url)
+    indexes(hockeystreams,4)
 elif mode == 2:
-    liveHqGames(url,username,password)
+    indexes(hockeystreams,7)
 elif mode == 3:
-    liveHdGames(url,username,password)
+    indexes(hockeystreams,10)
 elif mode == 4:
-    liveHdpGames(url,username,password)
+    populateGames(hqStreams)
 elif mode == 5:
-    liveHqLinks(url,username,password)
+    populateGames(hdStreams)
 elif mode == 6:
-    liveHdLinks(url,username,password)
+    populateGames(hdpStreams)
 elif mode == 7:
-    liveHdpLinks(url,username,password)
+    populateGames(hqArchives)
 elif mode == 8:
-    archiveIndex(url)
+    populateGames(hdArchives)
 elif mode == 9:
-    archiveHqGames(url,username,password,archiveDate)
-elif mode == 10:
-    archiveHdGames(url,username,password,archiveDate)
-elif mode == 11:
-    archiveHdpGames(url,username,password,archiveDate)
-elif mode == 12:
-    archiveHqLinks(url,username,password)
-elif mode == 13:
-    archiveHdLinks(url,username,password)
-elif mode == 14:
-    archiveHdpLinks(url,username,password)
+    populateGames(hdpArchives)
+##rss stuff
+##---------------------------------------------------
+
+##elif mode >= 10:
+##    import rssArchives
+##    if mode == 10:
+##        rssArchives.populateRSS('hq')
+##    elif mode == 11:
+##        rssArchives.populateRSS('hd')
+##    elif mode == 12:
+##        rssArchives.populateRSS('hdp')
+
+##---------------------------------------------------
+##end of rss stuff
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
