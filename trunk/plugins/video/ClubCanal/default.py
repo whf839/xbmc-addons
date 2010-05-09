928fafe37023f2d4dad7b16bb226a2ecd569f559
thebitjockey@thebitjockey.com <thebitjockey@thebitjockey.com@753fbab9-553a-0410-a42e-33068982fbc4>
2010-05-09 00:54:03 +0000

    [ClubCanal] - fixed: next page function

--- trunk/plugins/video/ClubCanal/default.py
+++ trunk/plugins/video/ClubCanal/default.py
@@ -1,26 +1,25 @@
-__plugin__ = "NakedCanal"
+__plugin__ = "ClubCanal"
 __authors__ = "thebitjockey"
 __credits__ = ""
-__version__ = "1.0.2"
+__version__ = "1.0.1"
 
 import urllib,urllib2,re,xbmcplugin,xbmcgui
 
 def CATEGORIES():
-        addDir('All Videos','http://www.nakedcanal.com/videos?c=0',1,'',1)
-        addDir('Classic Videos','http://www.nakedcanal.com/videos?c=4',1,'',1)
-        addDir('Erotic Videos','http://www.nakedcanal.com/videos?c=5',1,'',1)
-        addDir('Masturbation Videos','http://www.nakedcanal.com/videos?c=8',1,'',1)
-        addDir('Teen Videos','http://www.nakedcanal.com/videos?c=10',1,'',1)
-        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+        addDir('All Music Videos','http://www.clubcanal.com/videos?c=0',1,'',1)
+        addDir('Dance and Electronic Music Videos','http://www.clubcanal.com/videos?c=1',1,'',1)
+        addDir('Russian Dance Music Vide','http://www.clubcanal.com/videos?c=2',1,'',1)
+        addDir('Live Dance Music Videos','http://www.clubcanal.com/videos?c=3',1,'',1)
+        addDir('Chillout and Lounge Music Videos','http://www.clubcanal.com/videos?c=4',1,'',1)
+        addDir('How To and Interviews Videos','http://www.clubcanal.com/videos?c=5',1,'',1)
 
 def SORTMETHOD(url):
-        addDir('Most Recent Adult Videos',url+'&o=mr',2,'',1)
-        addDir('Being Watched Adult Videos',url+'&o=bw',2,'',1)
-        addDir('Most Viewed Adult Videos',url+'&o=mv',2,'',1)
-        addDir('Top Rated Adult Videos',url+'&o=tr',2,'',1)
-        addDir('Most Commented Adult Videos',url+'&o=md',2,'',1)
-        addDir('Top Favorite Adult Videos',url+'&o=tf',2,'',1)
-        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+        addDir('Most Recent Music Videos',url+'&o=mr',2,'',1)
+        addDir('Being Watched Music Videos',url+'&o=bw',2,'',1)
+        addDir('Most Viewed Music Videos',url+'&o=mv',2,'',1)
+        addDir('Top Rated Music Videos',url+'&o=tr',2,'',1)
+        addDir('Most Commented Music Videos',url+'&o=md',2,'',1)
+        addDir('Top Favorite Music Videos',url+'&o=tf',2,'',1)
         
                        
 def VIDEOLIST(url,page):
@@ -30,11 +29,9 @@ def VIDEOLIST(url,page):
         response.close()
         match=re.compile('<div class="video_box">\s+?<a href="/video/(.+?)/.+?"><img src=".+?" title="(.+?)" alt=".+?" width=".+?" height=".+?" id=".+?" />').findall(link)
         for videoid,name in match:
-                videourl=getvideourl(videoid)
-                addLink(name,videourl,'http://www.nakedcanal.com/media/videos/tmb/'+videoid+'/1.jpg',len(match))
+                addLink(name,'http://www.clubcanal.com/media/videos/flv/'+videoid+'.flv','http://www.clubcanal.com/media/videos/tmb/'+videoid+'/1.jpg',len(match))
         if (len(match) == 15):
             addDir('Next Page',url,2,'',page+1)
-        xbmcplugin.endOfDirectory(int(sys.argv[1]))
         
 def get_params():
         param=[]
@@ -57,7 +54,7 @@ def get_params():
 def addLink(name,url,iconimage,totalitems):
         ok=True
         liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name,"MPAA" : "XXX" } )
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz,totalItems=totalitems)
         return ok
 
@@ -66,59 +63,46 @@ def addDir(name,url,mode,iconimage,page):
         u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&page="+str(page)
         ok=True
         liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name, "MPAA" : "XXX" } )
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
         return ok
 
-def getvideourl(videoid):
-        url='http://www.nakedcanal.com/media/player/config.php?vkey='+videoid
-        req = urllib2.Request(url)
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
-        match=re.compile('<src>(.+?)\.flv</src>').findall(link)
-        videourl = match[0]+'.flv'
-        return videourl
+params=get_params()
+url=None
+name=None
+mode=None
+page=1
 
-def run():
+try:
+        url=urllib.unquote_plus(params["url"])
+except:
+        pass
+try:
+        name=urllib.unquote_plus(params["name"])
+except:
+        pass
+try:
+        mode=int(params["mode"])
+except:
+        pass
+try:
+        page=int(params["page"])
+except:
+        pass
+
+if mode==None or url==None or len(url)<1:
+        print ""
+        CATEGORIES()
+       
+elif mode==1:
+        print ""+url
+        SORTMETHOD(url)
         
-        params=get_params()
-        url=None
-        name=None
-        mode=None
-        page=1
+elif mode==2:
+        print ""+url
+        VIDEOLIST(url,page)
 
-        try:
-                url=urllib.unquote_plus(params["url"])
-        except:
-                pass
-        try:
-                name=urllib.unquote_plus(params["name"])
-        except:
-                pass
-        try:
-                mode=int(params["mode"])
-        except:
-                pass
-        try:
-                page=int(params["page"])
-        except:
-                pass
 
-        if mode==None or url==None or len(url)<1:
-                CATEGORIES()
-               
-        elif mode==1:
-                SORTMETHOD(url)
-                
-        elif mode==2:
-                VIDEOLIST(url,page)
 
-if __name__ == "__main__":
-        print sys.argv
-        if (len(sys.argv[2]) < 2) and (xbmcplugin.getSetting('hidewarning') == 'false'):
-                dialog = xbmcgui.Dialog()
-                if dialog.yesno(xbmc.getLocalizedString(30061), xbmc.getLocalizedString(30062), xbmc.getLocalizedString(30063), xbmc.getLocalizedString(30064), xbmc.getLocalizedString(30065), xbmc.getLocalizedString(30066)):
-                        run()
-        else:
-                run()
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
