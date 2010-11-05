c68f7a9d427430cb60601dc04cb4f158015dd849
nuka1195@gmail.com <nuka1195@gmail.com@753fbab9-553a-0410-a42e-33068982fbc4>
2010-11-05 14:56:02 +0000

    [script.module.xbmcaddon] - Version: 1.0.3
    -changed: static methods
    -changed: _settings_ and _language_ are no longer global
    
    * for xbox only

--- addons/script.module.xbmcaddon/lib/xbmcaddon.py
+++ addons/script.module.xbmcaddon/lib/xbmcaddon.py
@@ -8,21 +8,6 @@ import xbmc
 import re
 import locale
 
-# get current working directory
-cwd = os.getcwd()
-# check if we're at root folder of addon
-if ( not os.path.isfile( os.path.join( cwd, "addon.xml" ) ) ):
-    # we're not at root, assume resources/lib/
-    cwd = os.path.dirname( os.path.dirname( os.getcwd() ) )
-
-# language module
-_language_ = xbmc.Language( cwd ).getLocalizedString
-# settings module, try catch necessary as not all scripts have settings
-try:
-    _settings_ = xbmc.Settings( cwd )
-except:
-    _settings_ = None
-
 
 class Addon:
     """
@@ -41,11 +26,34 @@ class Addon:
             Initializer for passing the addon's id and setting addon info.
             Currently id is not used for xbmc4xbox.
         """
+        # get root dir
+        cwd = self._get_root_dir()
+        # get settings and language methods
+        self._get_methods( cwd )
         # TODO: do we want to use id for anything?
         # parse addon.xml and set all addon info
-        self._set_addon_info( id )
+        self._set_addon_info( cwd, id )
 
-    def _set_addon_info( self, id ):
+    def _get_root_dir( self ):
+        # get current working directory
+        cwd = os.getcwd()
+        # check if we're at root folder of addon
+        if ( not os.path.isfile( os.path.join( cwd, "addon.xml" ) ) ):
+            # we're not at root, assume resources/lib/
+            cwd = os.path.dirname( os.path.dirname( os.getcwd() ) )
+        # return result
+        return cwd
+
+    def _get_methods( self, cwd ):
+        # language module
+        self._language_ = xbmc.Language( cwd ).getLocalizedString
+        # settings module, try catch necessary as not all scripts have settings
+        try:
+            self._settings_ = xbmc.Settings( cwd )
+        except:
+            self._settings_ = None
+
+    def _set_addon_info( self, cwd, id ):
         # get source
         xml = open( os.path.join( cwd, "addon.xml" ), "r" ).read()
         # set addon.xml info into dictionary
@@ -85,8 +93,7 @@ class Addon:
         """
         return self._info[ id.lower() ]
 
-    @staticmethod
-    def getLocalizedString( id ):
+    def getLocalizedString( self, id ):
         """
             getLocalizedString(id) -- Returns the localized string as a unicode object.
 
@@ -95,10 +102,9 @@ class Addon:
             example:
               - locstr = self.Addon.getLocalizedString(id=30000)
         """
-        return _language_( id )
+        return self._language_( id )
 
-    @staticmethod
-    def getSetting( id ):
+    def getSetting( self, id ):
         """
             getSetting(id) -- Returns the value of a setting as a unicode object.
 
@@ -107,10 +113,9 @@ class Addon:
             example:
               - username = self.Addon.getSetting(id="username")
         """
-        return _settings_.getSetting( id )
+        return self._settings_.getSetting( id )
 
-    @staticmethod
-    def setSetting( id, value ):
+    def setSetting( self, id, value ):
         """
             setSetting(id, value) -- Sets a setting for this addon.
 
@@ -120,14 +125,13 @@ class Addon:
             example:
               - self.Addon.setSetting(id="username", value="nuka1195")
         """
-        _settings_.setSetting( id, value )
+        self._settings_.setSetting( id, value )
 
-    @staticmethod
-    def openSettings():
+    def openSettings( self ):
         """
             openSettings() -- Opens this addons settings dialog.
 
             example:
               - self.Addon.openSettings()
         """
-        _settings_.openSettings()
+        self._settings_.openSettings()
